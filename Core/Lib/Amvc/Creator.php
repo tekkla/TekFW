156d9dc19ac8a799c859dc57f51338eeb5ccca17
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-12 17:24:38 +0200

    Added missing core.content.nav as menu to app construction
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Amvc/Creator.php
+++ Core/Lib/Amvc/Creator.php
@@ -10,7 +10,7 @@ class Creator
 
     /**
      * List of secured app, which resides within the framework folder.
-     * 
+     *
      * @var array
      */
     private $secure_apps = [
@@ -21,7 +21,7 @@ class Creator
 
     /**
      * List of apps, which can get instances of secured apps.
-     * 
+     *
      * @var unknown
      */
     private $allow_secure_instance = [
@@ -46,7 +46,7 @@ class Creator
 
     /**
      * Get an unique app object
-     * 
+     *
      * @param string $name
      * @return App
      */
@@ -55,32 +55,32 @@ class Creator
         if (! is_bool($do_init)) {
             Throw new \InvalidArgumentException('Init flag for apps have to be of type boolean');
         }
-        
+
         // Make sure to have an app instance already
         if (! isset(self::$instances[$name])) {
-            
+
             // Create new app instance
             $this->create($name);
-            
+
             // Creation already did initiation
             $do_init = false;
         }
-        
+
         // Get clone of app
         $app = clone self::$instances[$name];
-        
+
         // Init this app instance
         if ($do_init) {
             $app->init();
         }
-        
+
         // Return referenc to app object in instance storage
         return $app;
     }
 
     /**
      * Get a singleton app object
-     * 
+     *
      * @param string $name
      * @param bool $do_init
      * @return App
@@ -89,7 +89,7 @@ class Creator
     {
         // Create app namespace and take care of secured apps.
         $class = in_array($name, $this->secure_apps) ? '\Core\AppsSec\\' . $name . '\\' . $name : '\Apps\\' . $name . '\\' . $name;
-        
+
         // Check for already existing instance of app
         // and create new instance when none is found
         if (! array_key_exists($name, self::$instances)) {
@@ -99,13 +99,14 @@ class Creator
                 'core.cfg',
                 'core.request',
                 'core.content.css',
-                'core.content.js'
+                'core.content.js',
+            	'core.content.nav'
             ];
-            
+
             $app = self::$instances[$name] = $this->di->instance($class, $args);
             $app->init();
         }
-        
+
         // Return app instance
         return self::$instances[$name];
     }
@@ -119,7 +120,7 @@ class Creator
                         if ($file == '..' || $file == '.' || $file == 'Core') {
                             continue;
                         }
-                        
+
                         $this->create($file);
                     }
                     closedir($dh);
@@ -132,10 +133,10 @@ class Creator
     {
         // Init app
         $cfg_app = $this->create($app_name)->getSettings();
-        
+
         // Get global config
         $cfg = $this->di['core.cfg'];
-        
+
         // Add default values for not set config
         foreach ($cfg_app as $key => $cfg_def) {
             // Set possible vaue from apps default config when no config was loaded from db
