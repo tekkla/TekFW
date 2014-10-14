cbf6769c29f94e5ec95e8f2b4a62a31f8f450bd3
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-15 01:31:49 +0200

    (New) Method to load permissions filtered by group idsfrom db.
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Security/Permission.php
+++ Core/Lib/Security/Permission.php
@@ -1,57 +1,97 @@
 <?php
 namespace Core\Lib\Security;
 
+use Core\Lib\Data\Database;
+
 /**
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- *        
+ *
  */
 class Permission
 {
 
-    static $permissions = [];
+	/**
+	 *
+	 * @var array
+	 */
+	private static $permissions = [];
 
-    /**
-     */
-    function __construct()
-    {}
+	/**
+	 *
+	 * @var Database
+	 */
+	private $db;
 
-    public function addPermission($appname, $permissions = [])
-    {
-        if ($permissions) {
-            
-            if (! is_array($permissions)) {
-                $var = (array) $permissions;
-            }
-            
-            foreach ($permissions as $perm) {
-                self::$permissions[$appname] = $perm;
-            }
-        }
-    }
+	public function __construct(Database $db)
+	{
+		$this->db = $db;
+	}
 
-    public function check($permission = [])
-    {
-        // Optimistic check. No permissions to check means check is ok
-        if (! $permission) {
-            return true;
-        }
-        
-        // Administrators are supermen :P.
-        if ($this->xxx['is_admin']) {
-            return true;
-        }
-        
-        if (! is_array($permission)) {
-            $permission = (array) $permission;
-        }
-        
-        // Check
-        return count(array_intersect($permission, self::$permissions['permissions'])) != 0 ? true : false;
-    }
+	/**
+	 * Adds one or more permissions to permissions list.
+	 *
+	 * @param string $app_name Name of permission related app
+	 * @param array $permissions One or more permissions to add
+	 */
+	public function addPermission($app_name, $permissions = [])
+	{
+		if ($permissions) {
 
-    public function getPermmissions()
-    {
-        return self::$permissions;
-    }
+			if (! is_array($permissions)) {
+				$var = (array) $permissions;
+			}
+
+			foreach ($permissions as $perm) {
+				self::$permissions[] = $app_name . '_' . $perm;
+			}
+		}
+	}
+
+	/**
+	 * Returns all or app related permissions
+	 *
+	 * @param string $app
+	 *
+	 * @return array
+	 */
+	public function getPermissions($app = '')
+	{
+		return $app ? self::$permissions[$app] : self::$permissions;
+	}
+
+	/**
+	 * Loads all permissions from DB which are mathing the groups argument.
+	 * Returns an empty array when groups argument is not set.
+	 *
+	 * @param unknown $group_id
+	 *
+	 * @return array
+	 */
+	public function loadPermission($groups = [])
+	{
+		// Queries without group IDs always results in an empty permission list
+		if (empty($groups)) {
+			return [];
+		}
+
+		// Convert group ID explicit into array
+		if (! is_array($groups)) {
+			$groups = (array) $groups;
+		}
+
+		// Create a prepared string and param array to use in query
+		$prepared = $this->db->prepareArrayQuery('group', $groups);
+
+		// Get and return the permissions
+		$this->db->query('SELECT DISTINCT permission FROM {db_prefix}permissions WHERE id_group IN (' . $prepared['sql'] . ')');
+
+		foreach ($prepared['values'] as $param => $value) {
+			$this->db->bindValue($param, $value);
+		}
+
+		$this->db->execute();
+
+		return $this->db->column();
+	}
 }
