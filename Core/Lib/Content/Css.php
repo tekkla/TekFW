ac2d2596d0d9bcad85cb45fceb408d4e1f72bd2d
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-14 21:07:25 +0200

    PSR Code rework done and missing comments added.
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Content/Css.php
+++ Core/Lib/Content/Css.php
@@ -15,214 +15,241 @@ use Core\Lib\Cfg;
 final class Css
 {
 
-    /**
-     * Storage of css objects
-     *
-     * @var array
-     */
-    private static $css = [];
+	/**
+	 * Storage of css objects
+	 *
+	 * @var array
+	 */
+	private static $css = [];
 
-    /**
-     * Type of css object
-     *
-     * @var string
-     */
-    private $type;
+	/**
+	 * Type of css object
+	 *
+	 * @var string
+	 */
+	private $type;
 
-    /**
-     * Css object content
-     *
-     * @var string
-     */
-    private $content;
+	/**
+	 * Css object content
+	 *
+	 * @var string
+	 */
+	private $content;
 
-    /**
-     *
-     * @var Cfg
-     */
-    private $cfg;
+	/**
+	 *
+	 * @var Cfg
+	 */
+	private $cfg;
 
-    public function __construct(Cfg $cfg)
-    {
-        $this->cfg = $cfg;
-    }
+	/**
+	 * Constructor
+	 *
+	 * @param Cfg $cfg
+	 */
+	public function __construct(Cfg $cfg)
+	{
+		$this->cfg = $cfg;
+	}
 
-    public function init()
-    {
-        // # Link basic framework css styles
+	/**
+	 * Initiates core css
+	 */
+	public function init()
+	{
+		// Add bootstrap main css file from cdn
+		$this->link('https://maxcdn.bootstrapcdn.com/bootstrap/' . $this->cfg->get('Core', 'bootstrap_version') . '/css/bootstrap.min.css');
 
-        // Add bootstrap main css file from cdn
-        $this->link('https://maxcdn.bootstrapcdn.com/bootstrap/' . $this->cfg->get('Core', 'bootstrap_version') . '/css/bootstrap.min.css');
+		// Add existing local user/theme related bootstrap file or load it from cdn
+		if (file_exists($this->cfg->get('Core', 'dir_css') . '/bootstrap-theme.css')) {
+			$this->link($this->cfg->get('Core', 'url_css') . '/bootstrap-theme.css');
+		}
+		else {
+			$this->link('https://maxcdn.bootstrapcdn.com/bootstrap/' . $this->cfg->get('Core', 'bootstrap_version') . '/css/bootstrap-theme.min.css');
+		}
 
-        // Add existing local user/theme related bootstrap file or load it from cdn
-        if (file_exists($this->cfg->get('Core', 'dir_css') . '/bootstrap-theme.css'))
-            $this->link($this->cfg->get('Core', 'url_css') . '/bootstrap-theme.css');
-        else
-            $this->link('https://maxcdn.bootstrapcdn.com/bootstrap/' . $this->cfg->get('Core', 'bootstrap_version') . '/css/bootstrap-theme.min.css');
+		// Add existing font-awesome font icon css file or load it from cdn
+		if (file_exists($this->cfg->get('Core', 'dir_css') . '/font-awesome-' . $this->cfg->get('Core', 'fontawesome_version') . '.min.css')) {
+			$this->link($this->cfg->get('Core', 'url_css') . '/font-awesome-' . $this->cfg->get('Core', 'fontawesome_version') . '.min.css');
+		}
+		else {
+			$this->link('https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
+		}
 
-            // Add existing font-awesome font icon css file or load it from cdn
-        if (file_exists($this->cfg->get('Core', 'dir_css') . '/font-awesome-' . $this->cfg->get('Core', 'fontawesome_version') . '.min.css'))
-            $this->link($this->cfg->get('Core', 'url_css') . '/font-awesome-' . $this->cfg->get('Core', 'fontawesome_version') . '.min.css');
-        else
-            $this->link('https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css');
+		// Add general TekFW css file
+		$this->link($this->cfg->get('Core', 'url_css') . '/Core.css');
+	}
 
-            // Add general TekFW css file
-        $this->link($this->cfg->get('Core', 'url_css') . '/Core.css');
-    }
+	/**
+	 * Adds a css object to the output queue
+	 *
+	 * @param Css $css
+	 */
+	public function &add()
+	{
+		self::$css[] = $this;
+		return $this;
+	}
 
-    /**
-     * Adds a css object to the output queue
-     *
-     * @param Css $css
-     */
-    public function &add()
-    {
-        self::$css[] = $this;
-        return $this;
-    }
+	/**
+	 * Compiles the objects in the output queue and adds them to SMFs $context.
+	 * Optional: If set in framework config, multiple css files will be
+	 * combined into one minified css file
+	 */
+	public function compile()
+	{
+		$files = [];
+		$inline = [];
 
-    /**
-     * Compiles the objects in the output queue and adds them to SMFs $context.
-     * Optional: If set in framework config, multiple css files will be
-     * combined into one minified css file
-     */
-    public function compile()
-    {
-        $files = [];
-        $inline = [];
+		$output = '';
 
-        $output = '';
+		/* @var $css Css */
+		foreach (self::$css as $css) {
 
-        /* @var $css Css */
-        foreach (self::$css as $css) {
-            switch ($css->getType()) {
-                case 'file':
-                    $files[] = $css->getCss();
-                    break;
+			switch ($css->getType()) {
+				case 'file':
+					$files[] = $css->getCss();
+					break;
 
-                case 'inline':
-                    $inline[] = $css->getCss();
-                    break;
-            }
-        }
+				case 'inline':
+					$inline[] = $css->getCss();
+					break;
+			}
+		}
 
-        // create script for minifier
-        if ($this->cfg->get('Core', 'css_minify')) {
-            foreach ($files as $file) {
-                if (strpos($file['filename'], BASEURL) !== false) {
-                    $board_parts = parse_url(BASEURL);
-                    $url_parts = parse_url($file['filename']);
+		// create script for minifier
+		if ($this->cfg->get('Core', 'css_minify')) {
 
-                    // Do not try to minify ressorces from external host
-                    if ($board_parts['host'] != $url_parts['host'])
-                        continue;
+			foreach ($files as $file) {
 
-                        // Store filename in minify list
-                    $files_to_min[] = '/' . $url_parts['path'];
-                }
-            }
+				if (strpos($file['filename'], BASEURL) !== false) {
 
-            if ($files_to_min) {
-                $_SESSION['min_css'] = $files_to_min;
-                $files = array(
-                    $this->cfg->get('Core', 'url_tools') . '/min/g=css'
-                );
-            }
-        }
+					$board_parts = parse_url(BASEURL);
+					$url_parts = parse_url($file['filename']);
 
-        ob_start();
+					// Do not try to minify ressorces from external host
+					if ($board_parts['host'] != $url_parts['host'])
+						continue;
 
-        foreach ($files as $file)
-            echo PHP_EOL . "\t" . '<link rel="stylesheet" type="text/css" href="', $file, '">';
+						// Store filename in minify list
+					$files_to_min[] = '/' . $url_parts['path'];
+				}
+			}
 
-        if ($inline)
-            echo '<style>', implode(PHP_EOL, $inline), '</style>';
+			if ($files_to_min) {
+				$_SESSION['min_css'] = $files_to_min;
+				$files = (array) $this->cfg->get('Core', 'url_tools') . '/min/g=css';
+			}
+		}
 
-        return ob_get_clean();
-    }
+		ob_start();
 
-    /**
-     * Sets objects type.
-     * Type can be "file" or "inline".
-     *
-     * @param string $type
-     * @throws Error
-     * @return \Core\Lib\Css
-     */
-    public function setType($type)
-    {
-        $types = [
-            'file',
-            'inline'
-        ];
+		foreach ($files as $file) {
+			echo PHP_EOL . "\t" . '<link rel="stylesheet" type="text/css" href="', $file, '">';
+		}
 
-        if (! in_array($type, $types))
-            Throw new \InvalidArgumentException('Css type must be "inline" or "file".');
+		if ($inline) {
+			echo '<style>', implode(PHP_EOL, $inline), '</style>';
+		}
 
-        $this->type = $type;
-        return $this;
-    }
+		return ob_get_clean();
+	}
 
-    /**
-     * Sets objects css content
-     *
-     * @param string $value
-     * @return \Core\Lib\Css
-     */
-    public function setCss($value)
-    {
-        $this->content = $value;
-        return $this;
-    }
+	/**
+	 * Sets objects type.
+	 * Type can be "file" or "inline".
+	 *
+	 * @param string $type
+	 *
+	 * @throws Error
+	 *
+	 * @return \Core\Lib\Css
+	 */
+	public function setType($type)
+	{
+		$types = [
+			'file',
+			'inline'
+		];
 
-    /**
-     * Get objects type (file or inline)
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
+		if (! in_array($type, $types)) {
+			Throw new \InvalidArgumentException('Css type must be "inline" or "file".');
+		}
 
-    /**
-     * Get objects css content
-     *
-     * @return string
-     */
-    public function getCss()
-    {
-        return $this->content;
-    }
+		$this->type = $type;
 
-    /**
-     * Creates and returns a link css object.
-     *
-     * @param string $url
-     * @return Css
-     */
-    public function &link($url)
-    {
-        $css = $this->di['core.content.css'];
-        $css->setType('file');
-        $css->setCss($url);
-        return $css->add();
-    }
+		return $this;
+	}
 
-    /**
-     * Creates and returns an inline css object
-     *
-     * @param string $styles
-     * @return \Core\Lib\Css
-     */
-    public function &inline($styles)
-    {
-        $css = $this->di['core.content.css'];
-        $css->setType('inline');
-        $css->setCss($styles);
-        return $css->add();
-    }
+	/**
+	 * Sets objects css content.
+	 *
+	 * @param string $value
+	 *
+	 * @return \Core\Lib\Css
+	 */
+	public function setCss($value)
+	{
+		$this->content = $value;
 
-    public function getStack()
-    {
-        return self::$css;
-    }
+		return $this;
+	}
+
+	/**
+	 * Get objects type (file or inline)
+	 */
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * Get objects css content
+	 *
+	 * @return string
+	 */
+	public function getCss()
+	{
+		return $this->content;
+	}
+
+	/**
+	 * Creates and returns a link css object.
+	 *
+	 * @param string $url
+	 *
+	 * @return Css
+	 */
+	public function &link($url)
+	{
+		$css = $this->di['core.content.css'];
+		$css->setType('file');
+		$css->setCss($url);
+
+		return $css->add();
+	}
+
+	/**
+	 * Creates and returns an inline css object
+	 *
+	 * @param string $styles
+	 *
+	 * @return \Core\Lib\Css
+	 */
+	public function &inline($styles)
+	{
+		$css = $this->di['core.content.css'];
+		$css->setType('inline');
+		$css->setCss($styles);
+
+		return $css->add();
+	}
+
+	/**
+	 * Returns the current stack off css commands
+	 */
+	public function getStack()
+	{
+		return self::$css;
+	}
 }
