cabc2d506e439d70d8586b564d041e9d286e9bf5
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-16 17:26:52 +0200

    Code rework
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Traits/AccessTrait.php
+++ Core/Lib/Traits/AccessTrait.php
@@ -4,15 +4,12 @@ namespace Core\Lib\Traits;
 /**
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- *        
+ *
  */
 trait AccessTrait
 {
-
-    protected function checkAccess($perms = [], $force = false)
-    {
-        return $this->di['core.sec.security']->checkAccess($perms, $force);
-    }
+	protected function checkAccess($perms = [], $force = false)
+	{
+		return $this->di['core.sec.security']->checkAccess($perms, $force);
+	}
 }
-
-?>
