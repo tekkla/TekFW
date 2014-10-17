cefa00ed730101d1dfe700bb3025530f64c2c936
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-17 16:34:52 +0200

    Fixed result of a noobish regex replace action -.-
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/AppsSec/Core/View/AdminView.php
+++ Core/AppsSec/Core/View/AdminView.php
@@ -6,36 +6,33 @@ use Core\Lib\Amvc\View;
 /**
  *
  * @author Michael
- *        
+ *
  */
 class AdminView extends View
 {
 
-    public function Index()
-    {
-        echo '<hTekFW Framework Config</h';
-		
+	public function Index()
+	{
 		echo '
-		<div class="row
-			<div class="col-sm-6
-				<div class="panel panel-default
-					<div class="panel-body
-						<a class="btn btn-default" href="', ->config, 
-        'Framework Config</
-						<h<stronApplications:</stron</h
-						<ul class="list-group';
-		
-		foreach ( ->loaded_apps as $app_name => $app )
-			
-        echo '
-							<li class="list-group-item clearfix', $app_name, ($appconfig_link ? '<a href="' . $appconfig_link . '" class="btn btn-default btn-xs pull-right<i class="fa fa-cog</</</l' : '');
-        
-        echo '
-						</u
-					</di
-				</di
-			</di
-		</di';
-    }
+        <h1>TekFW Framework Config</h1>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<a class="btn btn-default" href="', $this->config, '">Framework Config</a>
+						<h2><strong>Applications:</strong</h2>
+						<ul class="list-group">';
+
+		foreach ($this->loaded_apps as $app_name => $app) {
+			echo '<li class="list-group-item clearfix">', $app_name, ($app->config_link ? '<a href="' . $app->config_link . '" class="btn btn-default btn-xs pull-right"><i class="fa fa-cog"></i></a></li>' : '');
+		}
+
+		echo '
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>';
+	}
 }
 
--- Core/AppsSec/Core/View/ConfigView.php
+++ Core/AppsSec/Core/View/ConfigView.php
@@ -2,12 +2,10 @@
 namespace Core\AppsSec\Admin\View;
 
 use Core\Lib\Amvc\View;
-if (! defined('TEKFW'))
-    die('Cannot run without TekFW framework...');
 
 /**
  * Admin Config view
- * 
+ *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.d
  * @copyright 2014
  * @license MIT
@@ -17,10 +15,9 @@ if (! defined('TEKFW'))
 final class ConfigView extends View
 {
 
-    public function Config()
-    {
-		echo '<h' . ->icon . '&nbsp;' . ->app_name . '</h' . ->
-        form;
-    }
+	public function Config()
+	{
+		echo '<h1>' . $this->icon . '&nbsp;' . $this->app_name . '</h1>' . $this->form;
+	}
 }
 
