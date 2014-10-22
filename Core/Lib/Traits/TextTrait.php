ffe023ae7403d532de65a45272085e19401de6de
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-22 16:33:15 +0200

    Reformat / PSR
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Traits/TextTrait.php
+++ Core/Lib/Traits/TextTrait.php
@@ -10,23 +10,25 @@ use Core\Lib\Amvc\App;
  */
 trait TextTrait
 {
-    /**
-     * Get text from language service.
-     *
-     * @param string $key
-     * @param string $app
-     *
-     * @return string
-     */
-    public function txt($key)
-    {
-    	if ($this instanceof App)
-    		$app = $this->name;
-    	elseif (property_exists($this, 'app'))
-    		$app = $this->app->getName();
-    	else
-    		$app = 'Core';
 
-        return $this->di['core.content.lang']->getTxt($key, $app);
-    }
+	/**
+	 * Get text from language service.
+	 *
+	 * @param string $key
+	 * @param string $app
+	 *
+	 * @return string
+	 */
+	public function txt($key)
+	{
+		if ($this instanceof App) {
+			$app = $this->name;
+		} elseif (property_exists($this, 'app')) {
+			$app = $this->app->getName();
+		} else {
+			$app = 'Core';
+		}
+
+		return $this->di['core.content.lang']->getTxt($key, $app);
+	}
 }
