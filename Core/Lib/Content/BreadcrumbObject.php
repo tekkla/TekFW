19bd53acca750a5c1dfa351f75ffec6a445c66cc
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-10-23 21:12:21 +0200

    Added comments
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Content/BreadcrumbObject.php
+++ Core/Lib/Content/BreadcrumbObject.php
@@ -2,20 +2,47 @@
 namespace Core\Lib\Content;
 
 /**
+ * BreadcrumbObject
  *
- * @author Michael
+ * Class to use as element in Breadcrumb object
  *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @license MIT
+ * @copyright 2014
  */
 class BreadcrumbObject
 {
+	/**
+	 * Href url
+	 * @var string
+	 */
 	private $href = '';
 
+	/**
+	 * Inner text
+	 * @var string
+	 */
 	private $text = '';
 
+	/**
+	 * Title text
+	 * @var string
+	 */
 	private $title = '';
 
+	/**
+	 * Active flag
+	 * @var boolean
+	 */
 	private $active = false;
 
+	/**
+	 * Sets href to use in breadcrumb link
+	 *
+	 * @param string $href
+	 *
+	 * @return \Core\Lib\Content\BreadcrumbObject
+	 */
 	public function setHref($href)
 	{
 		$this->href = $href;
@@ -23,6 +50,13 @@ class BreadcrumbObject
 		return $this;
 	}
 
+	/**
+	 * Sets title to use on breadcrumb
+	 *
+	 * @param string $title
+	 *
+	 * @return \Core\Lib\Content\BreadcrumbObject
+	 */
 	public function setTitle($title)
 	{
 		$this->title = $title;
@@ -30,6 +64,13 @@ class BreadcrumbObject
 		return $this;
 	}
 
+	/**
+	 * Sets inner text
+	 *
+	 * @param string $text
+	 *
+	 * @return \Core\Lib\Content\BreadcrumbObject
+	 */
 	public function setText($text)
 	{
 		$this->text = $text;
@@ -37,6 +78,13 @@ class BreadcrumbObject
 		return $this;
 	}
 
+	/**
+	 * Sets active flag to be used on control creation
+	 *
+	 * @param string $active
+	 *
+	 * @return \Core\Lib\Content\BreadcrumbObject
+	 */
 	public function setActive($active=true)
 	{
 		$this->active = $active;
@@ -44,21 +92,41 @@ class BreadcrumbObject
 		return $this;
 	}
 
+	/**
+	 * Returns set href
+	 *
+	 * @return string
+	 */
 	public function getHref()
 	{
 		return $this->href;
 	}
 
+	/**
+	 * Returns set title
+	 *
+	 * @return string
+	 */
 	public function getTitle()
 	{
 		return $this->title;
 	}
 
+	/**
+	 * Returns set text
+	 *
+	 * @return string
+	 */
 	public function getText()
 	{
 		return $this->text;
 	}
 
+	/**
+	 * Returns active flag
+	 *
+	 * @return boolean
+	 */
 	public function getActive()
 	{
 		return $this->active;
