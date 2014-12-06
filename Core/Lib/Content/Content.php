57898459bfa4f0f420964f35cf2f2b8ee0a94f12
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-12-07 00:09:30 +0100

    Changed display of error messages.

--- Core/Lib/Content/Content.php
+++ Core/Lib/Content/Content.php
@@ -12,279 +12,270 @@ use Core\Lib\Amvc\Creator;
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
  * @licencse MIT
  * @copyright 2014
- * @version 0.1
  */
 class Content
 {
 
-	use \Core\Lib\Traits\TextTrait;
+    use \Core\Lib\Traits\TextTrait;
 
-	/**
-	 *
-	 * @var Cfg
-	 */
-	private $cfg;
+    /**
+     *
+     * @var Cfg
+     */
+    private $cfg;
 
-	/**
-	 *
-	 * @var Router
-	 */
-	private $router;
+    /**
+     *
+     * @var Router
+     */
+    private $router;
 
-	/**
-	 *
-	 * @var Creator
-	 */
-	private $app_creator;
+    /**
+     *
+     * @var Creator
+     */
+    private $app_creator;
 
-	/**
-	 *
-	 * @var string
-	 */
-	private $title;
+    /**
+     *
+     * @var string
+     */
+    private $title;
 
-	/**
-	 *
-	 * @var Meta
-	 */
-	public $meta;
+    /**
+     *
+     * @var Meta
+     */
+    public $meta;
 
-	/**
-	 *
-	 * @var Css
-	 */
-	public $css;
+    /**
+     *
+     * @var Css
+     */
+    public $css;
 
-	/**
-	 *
-	 * @var OpenGraph
-	 */
-	public $og;
+    /**
+     *
+     * @var OpenGraph
+     */
+    public $og;
 
-	/**
-	 *
-	 * @var Link
-	 */
-	public $link;
+    /**
+     *
+     * @var Link
+     */
+    public $link;
 
-	/**
-	 *
-	 * @var Javascript
-	 */
-	public $js;
+    /**
+     *
+     * @var Javascript
+     */
+    public $js;
 
-	/**
-	 *
-	 * @var Menu
-	 */
-	public $navbar;
+    /**
+     *
+     * @var Menu
+     */
+    public $navbar;
 
-	/**
-	 *
-	 * @var Breadcrumb
-	 */
-	public $breadcrumbs;
+    /**
+     *
+     * @var Breadcrumb
+     */
+    public $breadcrumbs;
 
-	/**
-	 *
-	 * @var string
-	 */
-	private $content;
+    /**
+     *
+     * @var string
+     */
+    private $content;
 
-	/**
-	 *
-	 * @var HtmlFactory
-	 */
-	private $html;
+    /**
+     *
+     * @var HtmlFactory
+     */
+    private $html;
 
-	/**
-	 *
-	 * @var Message
-	 */
-	public $msg;
+    /**
+     *
+     * @var Message
+     */
+    public $msg;
 
-	/**
-	 * Constructor
-	 *
-	 * @param Router $router
-	 * @param Cfg $cfg
-	 * @param Creator $app_creator
-	 * @param HtmlFactory $html
-	 * @param Menu $menu
-	 * @param Css $css
-	 * @param Javascript $js
-	 * @param Message $msg
-	 */
-	public function __construct(
-		Router $router,
-		Cfg $cfg,
-		Creator $app_creator,
-		HtmlFactory $html,
-		Menu $menu,
-		Css $css,
-		Javascript $js,
-		Message $msg
-	)
-	{
-		$this->router = $router;
-		$this->cfg = $cfg;
-		$this->app_creator = $app_creator;
-		$this->html = $html;
-		$this->navbar = $menu;
-		$this->js = $js;
-		$this->css = $css;
-		$this->msg = $msg;
+    /**
+     * Constructor
+     *
+     * @param Router $router
+     * @param Cfg $cfg
+     * @param Creator $app_creator
+     * @param HtmlFactory $html
+     * @param Menu $menu
+     * @param Css $css
+     * @param Javascript $js
+     * @param Message $msg
+     */
+    public function __construct(Router $router, Cfg $cfg, Creator $app_creator, HtmlFactory $html, Menu $menu, Css $css, Javascript $js, Message $msg)
+    {
+        $this->router = $router;
+        $this->cfg = $cfg;
+        $this->app_creator = $app_creator;
+        $this->html = $html;
+        $this->navbar = $menu;
+        $this->js = $js;
+        $this->css = $css;
+        $this->msg = $msg;
 
-		$this->meta = new Meta();
-		$this->og = new OpenGraph();
-		$this->link = new Link();
+        $this->meta = new Meta();
+        $this->og = new OpenGraph();
+        $this->link = new Link();
 
-		$this->breadcrumbs = new Breadcrumb();
+        $this->breadcrumbs = new Breadcrumb();
 
-		// Try to init possible content handler
-		if ($this->cfg->exists('Core', 'content_handler') && $this->router->isAjax()) {
+        // Try to init possible content handler
+        if ($this->cfg->exists('Core', 'content_handler') && $this->router->isAjax()) {
 
-			// Get instance of content handler app
-			$app = $this->app_creator->create($this->cfg->get('Core', 'content_handler'));
+            // Get instance of content handler app
+            $app = $this->app_creator->create($this->cfg->get('Core', 'content_handler'));
 
-			// Init method to call exists?
-			if (method_exists($app, 'initContentHandler')) {
-				$app->initContentHandler();
-			}
-		}
-	}
+            // Init method to call exists?
+            if (method_exists($app, 'initContentHandler')) {
+                $app->initContentHandler();
+            }
+        }
+    }
 
-	/**
-	 * Set pagetitle
-	 *
-	 * @param string $title
-	 *
-	 * @return \Core\Lib\Content\Content
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
+    /**
+     * Set pagetitle
+     *
+     * @param string $title
+     *
+     * @return \Core\Lib\Content\Content
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Set content to show
-	 *
-	 * @param string $content
-	 *
-	 * @return \Core\Lib\Content\Content
-	 */
-	public function setContent($content)
-	{
-		$this->content = $content;
+    /**
+     * Set content to show
+     *
+     * @param string $content
+     *
+     * @return \Core\Lib\Content\Content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Checks for a set config handler in web config
-	 *
-	 * @return boolean
-	 */
-	public function hasContentHandler()
-	{
-		return $this->cfg->exists('Core', 'content_handler');
-	}
+    /**
+     * Checks for a set config handler in web config
+     *
+     * @return boolean
+     */
+    public function hasContentHandler()
+    {
+        return $this->cfg->exists('Core', 'content_handler');
+    }
 
-	/**
-	 * Returns the name of config handler set in web config
-	 *
-	 * @return string
-	 */
-	public function getContenHandler()
-	{
-		return $this->cfg->get('Core', 'content_handler');
-	}
+    /**
+     * Returns the name of config handler set in web config
+     *
+     * @return string
+     */
+    public function getContenHandler()
+    {
+        return $this->cfg->get('Core', 'content_handler');
+    }
 
-	/**
-	 * Handles, enhances and returns the content of the page
-	 *
-	 * @throws \RuntimeException
-	 *
-	 * @return string
-	 */
-	public function getContent()
-	{
-		// Fill in content
-		try {
+    /**
+     * Handles, enhances and returns the content of the page
+     *
+     * @throws \RuntimeException
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        $this->content = '<div id="message"></div>' . $this->content;
 
-			// Try to run set content handler on non ajax request
-			if ($this->cfg->exists('Core', 'content_handler') && ! $this->router->isAjax()) {
+        // Fill in content
+        try {
 
-				// We need the name of the ContentCover app
-				$app_name = $this->cfg->get('Core', 'content_handler');
+            // Try to run set content handler on non ajax request
+            if ($this->cfg->exists('Core', 'content_handler') && ! $this->router->isAjax()) {
 
-				// Get instance of this app
-				$app = $this->appcreator->create($app_name);
+                // We need the name of the ContentCover app
+                $app_name = $this->cfg->get('Core', 'content_handler');
 
-				// Check for existing ContenCover method
-				if (! method_exists($app, 'runContentHandler')) {
-					Throw new \RuntimeException('You set the app "' . $app_name . '" as content handler but it lacks of method "runContentHandler()". Correct either the config or add the needed method to this app.');
-				}
+                // Get instance of this app
+                $app = $this->appcreator->create($app_name);
 
-				// Everything is all right. Run content handler by giving the current content to it.
-				$this->content = $app->runContentHandler($this->content);
-			}
+                // Check for existing ContenCover method
+                if (! method_exists($app, 'runContentHandler')) {
+                    Throw new \RuntimeException('You set the app "' . $app_name . '" as content handler but it lacks of method "runContentHandler()". Correct either the config or add the needed method to this app.');
+                }
 
-		} catch (\Exception $e) {
+                // Everything is all right. Run content handler by giving the current content to it.
+                $this->content .= $app->runContentHandler($this->content);
+            }
+        } catch (\Exception $e) {
 
-			// Add error message above content
-			$this->content = '<div class="alert alert-danger alert-dismissable">' . $e->getMessage() . '</div>';
+            // Add error message above content
+            $this->content .= '<div class="alert alert-danger alert-dismissable">' . $e->getMessage() . '</div>';
+        }
 
-		}
-
-		// Add framework status elements
-		$this->content .= '
+        // Add framework status elements
+        $this->content .= '
 		<div id="status"><i class="fa fa-spinner fa-spin"></i></div>
 		<div id="modal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true"></div>
 		<div id="debug"></div>
 		<div id="tooltip"></div>
 		<div id="scrolltotop"></div>';
 
-		// # Insert content
-		return $this->content;
-	}
+        // # Insert content
+        return $this->content;
+    }
 
-	/**
-	 * Renders and echoes template
-	 *
-	 * @param string $template Name of template
-	 * @param string $theme Name of theme template is from
-	 */
-	public function render($template='Index', $theme = 'Core')
-	{
-		$class = '\Themes\\' . $theme . '\\' . $template.  'Template';
-		$template = new $class($this->cfg, $this, $this->html);
+    /**
+     * Renders and echoes template
+     *
+     * @param string $template
+     *            Name of template
+     * @param string $theme
+     *            Name of theme template is from
+     */
+    public function render($template = 'Index', $theme = 'Core')
+    {
+        $class = '\Themes\\' . $theme . '\\' . $template . 'Template';
+        $template = new $class($this->cfg, $this, $this->html);
 
-		echo $template->render();
+        echo $template->render();
+    }
 
-	}
+    /**
+     * Returns set pagetitle
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * Returns set pagetitle
-	 *
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
-
-	/**
-	 * Returns sitename
-	 *
-	 * @return \Core\Lib\mixed
-	 */
-	public function getBrand()
-	{
-		return $this->cfg->get('Core', 'sitename');
-	}
+    /**
+     * Returns sitename
+     *
+     * @return \Core\Lib\mixed
+     */
+    public function getBrand()
+    {
+        return $this->cfg->get('Core', 'sitename');
+    }
 }
 
 
