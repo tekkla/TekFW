05007b944f215dba156f88ac8299903ecf7f55ff
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-12-07 23:45:58 +0100

    WIP Added FirePhp methods
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Traits/AnalyzeVarTrait.php
+++ Core/Lib/Traits/AnalyzeVarTrait.php
@@ -4,23 +4,58 @@ namespace Core\Lib\Traits;
 trait AnalyzeVarTrait
 {
 
-    public function var_dump($var, $exit = true)
-    {
-        var_dump($var);
+	public function fbLog($var) {
+		\FB::log($var);
+	}
 
+	public function fbInfo($var) {
+	    \FB::info($var);
+	}
+
+	public function fbWarning($var) {
+	    \FB::warn($var);
+	}
+
+	public function fbDump($key, $var)
+	{
+		\FB::dump($key, $var);
+	}
+
+    public function var_dump($var, $exit = false)
+    {
+        // Simple output to the browser
         if ($exit == true) {
+            var_dump($var);
             exit();
         }
+
+        // Adding to content output
+        if (!property_exists($this, 'di')) {
+            Throw new \RuntimeException('Analyzing var_dump needs access on the DI service container.');
+        }
+
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+
+        $this->di->get('core.content')->addDebug($output);
     }
 
-    public function print_r($var, $return = false, $exit = true)
+    public function print_r($var, $exit = false)
     {
-       $content = print_r($var, $return);
-
         if ($exit == true) {
+            echo '<pre>', print_r($var, true), '</pre>';
             exit();
         }
 
-        return $content;
+        if (!property_exists($this, 'di')) {
+            Throw new \RuntimeException('Analyzing print_r needs access on the DI service container.');
+        }
+
+        ob_start();
+        echo '<pre>', print_r($var, true), '</pre>';
+        $output = ob_get_clean();
+
+        $this->di->get('core.content')->addDebug($output);
     }
 }
