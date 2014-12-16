107de98d989d765446d01a905a8ff39a9227df1e
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-12-16 18:03:55 +0100

    TextTrait has now global access on $di to make sure translation can be used everywhere the trait is used.
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- Core/Lib/Traits/TextTrait.php
+++ Core/Lib/Traits/TextTrait.php
@@ -4,31 +4,37 @@ namespace Core\Lib\Traits;
 use Core\Lib\Amvc\App;
 
 /**
+ * Text Trait
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- *
+ * @copyright 2014
+ * @license MIT
  */
 trait TextTrait
 {
 
-	/**
-	 * Get text from language service.
-	 *
-	 * @param string $key
-	 * @param string $app
-	 *
-	 * @return string
-	 */
-	public function txt($key)
-	{
-		if ($this instanceof App) {
-			$app = $this->name;
-		} elseif (property_exists($this, 'app')) {
-			$app = $this->app->getName();
-		} else {
-			$app = 'Core';
-		}
+    /**
+     * Get text from language service.
+     *
+     * @param string $key
+     * @param string $app
+     *
+     * @return string
+     */
+    public function txt($key)
+    {
+        global $di;
 
-		return $this->di['core.content.lang']->getTxt($key, $app);
-	}
+        if ($this instanceof App) {
+            $app = $this->name;
+        }
+        elseif (property_exists($this, 'app')) {
+            $app = $this->app->getName();
+        }
+        else {
+            $app = 'Core';
+        }
+
+        return $di->get('core.content.lang')->getTxt($key, $app);
+    }
 }
