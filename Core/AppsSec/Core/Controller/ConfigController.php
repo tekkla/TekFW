151b02c44e4bd986cd20920f0adc0862f80816fd
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-04 16:08:21 +0100

    Config now works with new FormDesigner
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/AppsSec/Core/Controller/ConfigController.php
+++ Core/AppsSec/Core/Controller/ConfigController.php
@@ -2,219 +2,239 @@
 namespace Core\AppsSec\Core\Controller;
 
 use Core\Lib\Amvc\Controller;
-use Core\Lib\Amvc\App;
-use Core\Lib\Data\Data;
 
 class ConfigController extends Controller
 {
-	public $actions = array(
-		'*' => array(
-			'access' => 'admin',
-			'tools' => 'Form'
-		)
-	);
 
-	/**
-	 *
-	 * @param unknown $app_name
-	 * @throws \RuntimeException
-	 * @return void|boolean
-	 */
-	public function Config($app_name)
-	{
-		// Camelize app name becaus this parameter comes uncamelized from request handler
-		$app_name = $this->camelizeString($app_name);
+    public $actions = array(
+        '*' => array(
+            'access' => 'admin',
+            'tools' => 'Form'
+        )
+    );
 
-		// check permission
-		if (! $this->checkAccess($app_name . '_config')) {
-			Throw new \RuntimeException('No accessrights');
-		}
+    /**
+     *
+     * @param unknown $app_name
+     * @throws \RuntimeException
+     * @return void|boolean
+     */
+    public function Config($app_name)
+    {
+        // Camelize app name becaus this parameter comes uncamelized from request handler
+        $app_name = $this->camelizeString($app_name);
 
-		$post = $this->post->get();
+        // check permission
+        if (! $this->checkAccess($app_name . '_config')) {
+            Throw new \RuntimeException('No accessrights');
+        }
 
-		// save process
-		if ($post) {
-			$this->model->saveConfig($post);
+        $data = $this->post->get();
 
-			if ($this->model->hasNoErrors()) {
-				$this->messagesuccess($this->txt('config_saved'));
-				$redir_url = $this->router->url($this->router->getCurrentRoute(), array(
-					'app_name' => $this->uncamelizeString($app_name)
-				));
-				$this->redirectExit($redir_url);
-				return;
-			}
-		}
+        // save process
+        if ($data) {
 
-		// config headarea
-		$this->setVar(array(
-			'app_name' => $this->txt('name', $app_name),
-			'icon' => $this->html->create('Elements\Icon')->useIcon('cog')
-		));
+            $this->model->saveConfig($data);
 
-		// Load the app's config data
-		if ($this->model->hasNoData()) {
-			$this->model->loadByApp($app_name);
-		}
+            if (!$data->hasErrors()) {
+                $this->content->msg->success($this->txt('config_saved'));
+                $redir_url = $this->url($this->router->getCurrentRoute(), [
+                    'app_name' => $this->uncamelizeString($app_name)
+                ]);
 
-		// Use form designer
-		$form = $this->getFormDesigner();
+                $this->redirectExit($redir_url);
+                return;
+            }
+        }
 
-		// Set forms action route
-		$form->setActionRoute($this->router->getCurrentRoute(), array(
-			'app_name' => $this->uncamelizeString($app_name)
-		));
+        // config headarea
+        $this->setVar(array(
+            'app_name' => $this->txt('name', $app_name),
+            'icon' => $this->html->create('Elements\Icon')
+                ->useIcon('cog')
+        ));
 
-		// The app name is always needed
-		$form->createElement('hidden', 'app')->setValue($app_name);
+        // Load the app's config data
+        if (empty($data)) {
+            $data = $this->model->loadByApp($app_name);
+        }
 
-		// storage for active group
-		$groupname = '';
+        // Use form designer
+        $form = $this->getFormDesigner();
 
-		// Get the config definition from app
-		$app = $this->di['core.amvc.creator']->create($app_name);
-		$app_cfg = $app->getConfig();
+        $form->attachContainer($data);
 
-		// controls for each config key will be created as a loop
-		foreach ($this->model->data as $cfg_key => $cfg_value) {
+        // Set forms action route
+        $form->setActionRoute($this->router->getCurrentRoute(), array(
+            'app_name' => $this->uncamelizeString($app_name)
+        ));
 
-			if ($cfg_key == 'app' || $cfg_key == 'btn_submit') {
-				continue;
-			}
+        $group = $form->addGroup();
 
-			// Get the config definition of this config key
-			// $cfg_def = Lib::toObject($app_cfg[$cfg_key]);
-			$cfg_def = new Data($app_cfg[$cfg_key]);
+        // Add hidden app control
+        $group->addControl('hidden', 'app_name')->setValue($app_name);
 
-			// add a group header if the controls group is
-			// different the one stored as active group
-			if ($cfg_def->group !== $groupname) {
+        // storage for active group
+        $groupname = '';
 
-				$group = $form->openGroup($cfg_def->group);
-				$group->setHeading($this->txt($this->uncamelizeString('app_' . ($app_name == 'admin' ? 'web' : $app_name) . '_cfg_group_' . $cfg_def->group)), 3);
-				$group->addCss('app-admin-config-group');
-				$group->noRow();
+        // Get the config definition from app
+        $app = $this->di['core.amvc.creator']->create($app_name);
+        $app_cfg = $app->getConfig();
 
-				// Set this group as active group
-				$groupname = $cfg_def->group;
-			}
+        // controls for each config key will be created as a loop
+        foreach ($data as $key => $fld) {
 
-			// Is this a control with more settings or only the controltype
-			$control_type = is_object($cfg_def->control) ? $cfg_def->control{0} : $cfg_def->control;
+            if ($key == 'app_name') {
+                continue;
+            }
 
-			// Create control object
-			$control = $form->createElement($control_type, $cfg_key);
+            $cfg = $app_cfg[$key];
 
-			// Are there attributes to add?
-			if (is_object($cfg_def->control) && isset($cfg_def->control{1}) && is_object($cfg_def->control{1})) {
+            // add a group header if the controls group is
+            // different the one stored as active group
+            if ($cfg['group'] !== $groupname) {
 
-				// Add all attributes to the control
-				foreach ($cfg_def->control{1} as $attr => $val) {
-					$control->addAttribute($attr, $val);
-				}
-			}
+                $group = $form->addGroup();
 
-			// Create controls
-			switch ($control_type) {
-				case 'textarea':
-				case 'text':
-					if (isset($cfg_def->default) && ! isset($cfg_deftranslate)) {
-						$cfg_def->default = $this->txt($app_name . '_' . $cfg_def->default);
-					}
-					break;
+                $group->addElement('Elements\Heading', [
+                    'setInner' => $this->txt($this->uncamelizeString('cfg_group_' . $cfg['group'], $app_name)),
+                    'setSize' => 3
+                ]);
 
-				// Create datasource driven controls
-				case 'optiongroup':
-				case 'select':
-				case 'multiselect':
+                // Set this group as active group
+                $groupname = $cfg['group'];
+            }
 
-					if (! isset($cfg_def->data) || isset($cfg_def->data) && ! is_object($cfg_def->data)) {
-						Throw new \RuntimeException('No or not correct set data definition.');
-					}
+            // Is this a control with more settings or only the controltype
+            $control_type = is_array($cfg['control']) ? $cfg['control'][0] : $cfg['control'];
 
-					// Load optiongroup datasource type
-					switch ($cfg_def->data->{0}) {
-						// Type: model
-						case 'model':
-							list ($model_app, $model_name, $model_action) = explode('::', $cfg_def->data->{1});
-							$datasource = App::create($model_app)->getModel($model_name);
-							break;
+            // Create control object
+            $control = $group->addControl($control_type, $key);
 
-						// Type: array
-						case 'array':
-							$datasource = $cfg_def->data->{1};
-							break;
+            // Are there attributes to add?
+            if (is_array($cfg['control']) && isset($cfg['control'][1]) && is_array($cfg['control'][1])) {
 
-						// Datasource has to be of type array or model. All other will result in an exception
-						default:
-							Throw new \RuntimeException('Wrong or none datasource set for control "' . $cfg_key . '" of type "' . $cfg_def->control . '"');
-					}
+                // Add all attributes to the control
+                foreach ($cfg['control'][1] as $attr => $val) {
+                    $control->addAttribute($attr, $val);
+                }
+            }
 
-					// if no bound column number is set, set default to column 0
-					if (! isset($cfg_def->data->{2})) {
-						$cfg_def->data->{2} = 0;
-					}
+            // Create controls
+            switch ($control_type) {
+                case 'textarea':
+                case 'text':
 
-					// Create the list of options
-					foreach ($datasource as $key => $val) {
+                    if (isset($cfg['translate'])) {
+                        $cfg['value'] = $this->txt($app_name . '_' . $fld->getValue());
+                    }
 
-						$option_value = $cfg_def->data->{2} == 0 ? $key : $val;
+                    if ($control_type == 'textarea') {
+                        $control->setInner($fld->getValue());
+                    }
+                    else {
+                        $control->setValue($fld->getValue());
+                    }
 
-						$option = $control->createOption();
-						$option->setInner($val);
-						$option->setValue($option_value);
+                    break;
 
-						if (is_object($cfg_value)) {
-							foreach ($cfg_value as $k => $v) {
-								if (($control_type == 'multiselect' && $v == html_entity_decode($option_value)) || ($control_type == 'optiongroup' && ($cfg_def->data->{2} == 0 && $k == $option_value) || ($cfg_def->data->{2} == 1 && $v == $option_value))) {
-									$option->isSelected(1);
-									unset($cfg_value{$k});
-									continue;
-								}
-							}
-						}
-						else {
+                // Create datasource driven controls
+                case 'optiongroup':
+                case 'select':
+                case 'multiselect':
 
-							// this is for simple select control
-							if (($cfg_def->data->{2} == 0 && $key === $cfg_value) || ($cfg_def->data->{2} == 1 && $val == $cfg_value)) {
-								$option->isSelected(1);
-							}
-						}
-					}
+                    if (! isset($cfg['data']) || isset($cfg['data']) && ! is_array($cfg['data'])) {
+                        Throw new \RuntimeException('No or not correct set data definition.');
+                    }
 
-					break;
+                    // Load optiongroup datasource type
+                    switch ($cfg['data'][0]) {
 
-				case 'switch':
-					if ($cfg_value == 1) {
-						$control->switchOn();
-					}
-					break;
+                        // DataType: model
+                        case 'model':
+                            list ($model_app, $model_name, $model_action) = explode('::', $cfg['data'][1]);
+                            $datasource = $this->di['core.amvc.creator']->create($model_app)->getModel($model_name);
+                            break;
 
-				default:
-					if (! $control->checkAttribute('size')) {
-						$control->setSize(55);
-					}
+                        // DataType: array
+                        case 'array':
+                            $datasource = $cfg['data'][1];
+                            break;
 
-					break;
-			}
+                        // Datasource has to be of type array or model. All other will result in an exception
+                        default:
+                            Throw new \RuntimeException('Wrong or none datasource set for control "' . $key . '" of type "' . $cfg['control'] . '"');
+                    }
 
-			$txt = $this->uncamelizeString('cfg_' . $cfg_key);
-			$app = $app_name == 'admin' ? 'web' : $app_name;
+                    // if no bound column number is set, set default to column 0
+                    if (! isset($cfg['data'][2])) {
+                        $cfg['data'][2] = 0;
+                    }
 
-			$control->setLabel($this->txt($txt, $app));
-			$control->setDescription($this->txt($txt . '_desc', $app));
-		}
+                    // Create the list of options
+                    foreach ($datasource as $ds_key => $ds_val) {
 
-		$this->setVar('form', $form);
+                        $option_value = $cfg['data'][2] == 0 ? $ds_key : $ds_val;
 
-		// Add linktreee
-		$this->content->breadcrumbs->createItem('TekFW Admincenter', $this->router->url('core_admin'));
+                        $option = $control->createOption();
+                        $option->setInner($ds_val);
+                        $option->setValue($option_value);
 
-		$this->content->breadcrumbs->createActiveItem($this->txt('name', $app_name));
-	}
 
-	public function Reconfigure($app_name)
-	{
-		$this->model->rewriteConfig($app_name);
-	}
+                            if (is_array($fld->getValue())) {
+                                foreach ($fld->getValue() as $k => $v) {
+                                    if (($control_type == 'multiselect' && $v == html_entity_decode($option_value)) || ($control_type == 'optiongroup' && ($cfg['data'][2] == 0 && $k == $option_value) || ($cfg['data'][2] == 1 && $v == $option_value))) {
+                                        $option->isSelected(1);
+                                        continue;
+                                    }
+                                }
+                            }
+                            else {
+
+                                // this is for simple select control
+                                if (($cfg['data'][2] == 0 && $ds_key === $fld->getValue()) || ($cfg['data'][2] == 1 && $ds_val == $fld->getValue())) {
+                                    $option->isSelected(1);
+                                }
+                            }
+
+                    }
+
+                    break;
+
+                case 'switch':
+
+                    if ($fld->getValue() == 1) {
+                        $control->switchOn();
+                    }
+                    break;
+
+                default:
+                    if (! $control->checkAttribute('size')) {
+                        $control->setSize(55);
+                    }
+
+                    $control->setValue($fld->getValue());
+
+                    break;
+            }
+
+            $txt = $this->uncamelizeString('cfg_' . $key);
+            $app = $app_name == 'admin' ? 'web' : $app_name;
+
+            $control->setLabel($this->txt($txt, $app));
+            $control->setDescription($this->txt($txt . '_desc', $app));
+        }
+
+        $this->setVar('form', $form);
+
+        // Add linktreee
+        $this->content->breadcrumbs->createItem('TekFW Admincenter', $this->router->url('core_admin'));
+
+        $this->content->breadcrumbs->createActiveItem($this->txt('name', $app_name));
+    }
+
+    public function Reconfigure($app_name)
+    {
+        $this->model->rewriteConfig($app_name);
+    }
 }
