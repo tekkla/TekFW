7166ce08fcc4508905ccf70e72befbab519fe9a5
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-11 13:30:15 +0100

    Fixed bug that prevents returning flash messages
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/Lib/Content/Message.php
+++ Core/Lib/Content/Message.php
@@ -7,159 +7,159 @@ use Core\Lib\Http\Session;
  * Message class for flash messages.
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- * @copyright 2014
+ * @copyright 2015
  * @license MIT
- * @package TekFW
- * @subpackage Lib
  */
 class Message
 {
-	/**
-	 *
-	 * @var Session
-	 */
-	private $session;
 
-	/**
-	 * Constructor
-	 *
-	 * @param Session $session
-	 */
-	public function __construct(Session $session)
-	{
-		$this->session = $session;
-	}
+    /**
+     *
+     * @var Session
+     */
+    private $session;
 
-	/**
-	 * Generice function to create a message object
-	 * Called by public mapper methods
-	 *
-	 * @param string $type
-	 * @param string $message
-	 * @param string $fadeout
-	 *
-	 * @return MessageObject
-	 */
-	private function generic($type, $message, $fadeout = true)
-	{
-		$msg = new MessageObject();
+    /**
+     * Constructor
+     *
+     * @param Session $session
+     */
+    public function __construct(Session $session)
+    {
+        $this->session = $session;
+    }
 
-		$msg->setType($type);
-		$msg->setMessage($message);
-		$msg->setFadeout($fadeout);
+    /**
+     * Generice function to create a message object
+     * Called by public mapper methods
+     *
+     * @param string $type
+     * @param string $message
+     * @param string $fadeout
+     *
+     * @return MessageObject
+     */
+    private function generic($type, $message, $fadeout = true)
+    {
 
-		$this->add($msg);
+        $msg = new MessageObject();
 
-		return $msg;
-	}
+        $msg->setType($type);
+        $msg->setMessage($message);
+        $msg->setFadeout($fadeout);
 
-	/**
-	 * Adds a message object to the message storage in session
-	 *
-	 * @param MessageObject $msg
-	 */
-	public function add(MessageObject &$msg)
-	{
-		$this->session->add('messages', $msg);
-	}
+        $this->add($msg);
 
-	/**
-	 * Creates "primary" message and returns reference to this messages.
-	 *
-	 * @param string $message Message content
-	 * @param bool $fadeout Automatic fadeout. Set to false dto disable.
-	 *
-	 * @return MessageObject
-	 */
-	public function primary($message, $fadeout = true)
-	{
-		return $this->generic('primary', $message, $fadeout);
-	}
+        return $msg;
+    }
 
-	/**
-	 * Creates "succcess" message and returns reference to this messages.
-	 *
-	 * @param string $message Message content
-	 * @param bool $fadeout Automatic fadeout. Set to false dto disable.
-	 *
-	 * @return MessageObject
-	 */
-	public function success($message, $fadeout = true)
-	{
-		return $this->generic('success', $message, $fadeout);
-	}
+    /**
+     * Adds a message object to the message storage in session
+     *
+     * @param MessageObject $msg
+     */
+    public function add(MessageObject &$msg)
+    {
+        $this->session->add('messages', $msg);
+    }
 
-	/**
-	 * Creates "info" message and returns reference to this messages.
-	 *
-	 * @param string $message Message content
-	 * @param bool $fadeout Automatic fadeout. Set to false dto disable.
-	 *
-	 * @return MessageObject
-	 */
-	public function info($message, $fadeout = true)
-	{
-		return $this->generic('info', $message, $fadeout);
-	}
+    /**
+     * Creates "primary" message and returns reference to this messages.
+     *
+     * @param string $message Message content
+     * @param bool $fadeout Automatic fadeout. Set to false dto disable.
+     *
+     * @return MessageObject
+     */
+    public function primary($message, $fadeout = true)
+    {
+        return $this->generic('primary', $message, $fadeout);
+    }
 
-	/**
-	 * Creates "warning" message and returns reference to this messages.
-	 *
-	 * @param string $message Message content
-	 * @param bool $fadeout Automatic fadeout. Set to false dto disable.
-	 *
-	 * @return MessageObject
-	 */
-	public function warning($message, $fadeout = true)
-	{
-		return $this->generic('warning', $message, $fadeout);
-	}
+    /**
+     * Creates "succcess" message and returns reference to this messages.
+     *
+     * @param string $message Message content
+     * @param bool $fadeout Automatic fadeout. Set to false dto disable.
+     *
+     * @return MessageObject
+     */
+    public function success($message, $fadeout = true)
+    {
+        return $this->generic('success', $message, $fadeout);
+    }
 
-	/**
-	 * Creates "danger" message and returns reference to this messages.
-	 *
-	 * @param string $message Message content
-	 * @param bool $fadeout Automatic fadeout. Set to false dto disable.
-	 *
-	 * @return MessageObject
-	 */
-	public function danger($message, $fadeout = true)
-	{
-		return $this->generic('danger', $message, $fadeout);
-	}
+    /**
+     * Creates "info" message and returns reference to this messages.
+     *
+     * @param string $message Message content
+     * @param bool $fadeout Automatic fadeout. Set to false dto disable.
+     *
+     * @return MessageObject
+     */
+    public function info($message, $fadeout = true)
+    {
+        return $this->generic('info', $message, $fadeout);
+    }
 
-	/**
-	 * Checks for existing messages
-	 */
-	public static function checkMessages()
-	{
-		return $this->session->exists('messages');
-	}
+    /**
+     * Creates "warning" message and returns reference to this messages.
+     *
+     * @param string $message Message content
+     * @param bool $fadeout Automatic fadeout. Set to false dto disable.
+     *
+     * @return MessageObject
+     */
+    public function warning($message, $fadeout = true)
+    {
+        return $this->generic('warning', $message, $fadeout);
+    }
 
-	/**
-	 * Returns set messages and resets the the message storage.
-	 * If no message is set the method returns boolean false.
-	 */
-	public function getMessages()
-	{
-		if (!$this->session->exists('message')){
-			return [];
-		}
+    /**
+     * Creates "danger" message and returns reference to this messages.
+     *
+     * @param string $message Message content
+     * @param bool $fadeout Automatic fadeout. Set to false dto disable.
+     *
+     * @return MessageObject
+     */
+    public function danger($message, $fadeout = true)
+    {
+        return $this->generic('danger', $message, $fadeout);
+    }
 
-		$messages = $this->session->get('messages');
+    /**
+     * Checks for existing messages
+     */
+    public static function checkMessages()
+    {
+        return $this->session->exists('messages');
+    }
 
-		if ($messages) {
-			$this->resetMessages();
-		}
+    /**
+     * Returns set messages and resets the the message storage.
+     * If no message is set the method returns boolean false.
+     */
+    public function getMessages()
+    {
+        if (! $this->session->exists('messages')) {
+            return [];
+        }
 
-		return $messages;
-	}
+        $messages = $this->session->get('messages');
 
-	/**
-	 * Resets messages in session
-	 */
-	public static function resetMessages()
-	{
-		$this->session->remove('messages');
-	}
+        if ($messages) {
+            $this->resetMessages();
+        }
+
+        return $messages;
+    }
+
+    /**
+     * Resets messages in session
+     */
+    public function resetMessages()
+    {
+        $this->session->remove('messages');
+    }
 }
