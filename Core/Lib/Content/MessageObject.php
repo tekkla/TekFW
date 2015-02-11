27a1b4844a0343dfbbb400a142a191b63f46d9a8
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-11 23:21:53 +0100

    New dismissable flag to switch use of dismiss-button on or off.
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/Lib/Content/MessageObject.php
+++ Core/Lib/Content/MessageObject.php
@@ -5,97 +5,145 @@ namespace Core\Lib\Content;
  * Message class for flash messages.
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- * @copyright 2014
+ * @copyright 2015
  * @license MIT
- * @package TekFW
- * @subpackage Lib
  */
 class MessageObject
 {
-	/**
-	 * Sets message content
-	 *
-	 * @param string $message
-	 *
-	 * @return \Core\Lib\Message
-	 */
-	public function setMessage($message)
-	{
-		$this->message = $message;
 
-		return $this;
-	}
+    /**
+     *
+     * @var string
+     */
+    private $message;
 
-	/**
-	 * Returns message text
-	 *
-	 * @return string
-	 */
-	public function getMessage()
-	{
-		return $this->message;
-	}
+    /**
+     *
+     * @var string
+     */
+    private $type = 'default';
 
-	/**
-	 * Sets message type
-	 *
-	 * @param string $type
-	 *
-	 * @throws NoValidParameterError
-	 *
-	 * @return \Core\Lib\Message
-	 */
-	public function setType($type)
-	{
-		$types = [
-			'primary',
-			'success',
-			'info',
-			'warning',
-			'danger',
-			'default'
-		];
+    /**
+     *
+     * @var boolean
+     */
+    private $fadeout = true;
 
-		if (! in_array($type, $types)) {
-			Throw new \InvalidArgumentException('Wrong type set for message.');
-		}
+    /**
+     *
+     * @var boolean
+     */
+    private $dismissable = true;
 
-		$this->type = $type;
 
-		return $this;
-	}
+    /**
+     * Sets message content
+     *
+     * @param string $message
+     *
+     * @return \Core\Lib\Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
 
-	/**
-	 * Returns set message type
-	 *
-	 * @return string
-	 */
-	public function getType()
-	{
-		return $this->type;
-	}
+        return $this;
+    }
 
-	/**
-	 * Switches fadeout on or off
-	 *
-	 * @param bool $fadeout
-	 *
-	 * @return \Core\Lib\Message
-	 */
-	public function setFadeout($fadeout)
-	{
-		$this->fadeout = is_bool($fadeout) ? $fadeout : false;
+    /**
+     * Returns message text
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
 
-		return $this;
-	}
+    /**
+     * Sets message type
+     *
+     * @param string $type
+     *
+     * @throws NoValidParameterError
+     *
+     * @return \Core\Lib\Message
+     */
+    public function setType($type)
+    {
+        $types = [
+            'primary',
+            'success',
+            'info',
+            'warning',
+            'danger',
+            'default'
+        ];
 
-	/**
-	 * Returns set fadeout time
-	 *
-	 * @return boolean
-	 */
-	public function getFadeout()
-	{
-		return $this->fadeout;
-	}
+        if (! in_array($type, $types)) {
+            Throw new \InvalidArgumentException('Wrong type set for message.');
+        }
+
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Returns set message type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Switches fadeout on or off
+     *
+     * @param bool $fadeout
+     *
+     * @return \Core\Lib\Message
+     */
+    public function setFadeout($fadeout)
+    {
+        $this->fadeout = is_bool($fadeout) ? $fadeout : false;
+
+        return $this;
+    }
+
+    /**
+     * Returns set fadeout time
+     *
+     * @return boolean
+     */
+    public function getFadeout()
+    {
+        return $this->fadeout;
+    }
+
+    /**
+     * Switches dismissable button on/off
+     *
+     * @param bool $dismissable
+     *
+     * @return \Core\Lib\Message
+     */
+    public function setDismissable($dismissable)
+    {
+        $this->dismissable = is_bool($dismissable) ? $dismissable : false;
+
+        return $this;
+    }
+
+    /**
+     * Returns set dismissable flag.
+     *
+     * @return boolean
+     */
+    public function getDismissable()
+    {
+        return $this->dismissable;
+    }
 }
