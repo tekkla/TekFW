226079faafca0d379ae0757d684a5a8494c106b3
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-22 23:25:25 +0100

    Moved dispatch code to Content class
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- index.php
+++ index.php
@@ -154,139 +154,26 @@ try {
     $creator->autodiscover($app_dirs);
 
     // --------------------------------------
-    // 7. Run router to get match
+    // 7. Create content
     // --------------------------------------
+    $content->create();
 
-    /* @var $router \Core\Lib\Http\Router */
-    $router = $di->get('core.http.router');
+    // @TODO remove this later!
+    $fb = [
+        'Runtime' => $timer->getDiff() . 's',
+        'Permissions' => $di->get('core.sec.permission')->getPermissions(),
+        'Admin' => $di->get('core.sec.user.current')->isAdmin(),
+    ];
 
-    // Match request against stored routes
-    $router->match();
+    $debug = $content->getDebug();
 
-    // --------------------------------------
-    // 8. Run called app
-    // --------------------------------------
-
-    // Try to use appname provided by router
-    $app_name = $router->getApp();
-
-    // No app by request? Try to get default app from config or set Core as
-    // default app
-    if (! $app_name) {
-        $app_name = $config->exists('Core', 'default_app') ? $config->get('Core', 'default_app') : 'Core';
+    if ($debug) {
+        $fb['Content debug'] = $debug;
     }
 
-    // Start with factoring the requested app
+    \FB::log($fb);
 
-    /* @var $app \Core\Lib\Amvc\App */
-    $app = $creator->create($app_name);
-
-    /**
-     * Each app can have it's own start procedure.
-     * This procedure is used to
-     * init apps with more than the app creator does. To use this feature the
-     * app needs run() method in it's main file.
-     */
-    if (method_exists($app, 'run')) {
-        $app->run();
-    }
-
-    // Get name of requested controller
-    $controller_name = $router->getCtrl();
-
-    // Set controller name to "Index" when no controller name has been returned
-    // from request handler
-    if (! $controller_name) {
-        $controller_name = 'Index';
-    }
-
-    // Load controller object
-    $controller = $app->getController($controller_name);
-
-    // Which controller action has to be run?
-    $action = $router->getAction();
-
-    // No action => use Index as default
-    if (! $action) {
-        $action = 'Index';
-    }
-
-    // Are there parameters to pass to run method?
-    $params = $router->getParam();
-
-    // Run controller and process result.
-    if ($router->isAjax()) {
-
-        try {
-
-            // Result will be processed as ajax command list
-            $controller->ajax($action, $params);
-        }
-        catch (Exception $e) {
-
-            $di->get('core.error')->handleException($e, false, true);
-        }
-
-        // Send cache preventing headers and set content type
-        header('Cache-Control: no-cache, must-revalidate');
-        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-        header('Content-type: application/json');
-
-        // Run ajax processor
-        echo $di->get('core.ajax')->process();
-
-        // End end here
-        exit();
-    }
-    else {
-
-        Try {
-
-            // Run controller and store result
-            $result = $controller->run($action, $params);
-
-            // No content created? Check app for onEmpty() event which maybe gives us content.
-            if (empty($result) && method_exists($app, 'onEmpty')) {
-                $result = $app->onEmpty();
-            }
-
-            // Append content provided by apps onBefore() event method
-            if (method_exists($app, 'onBefore')) {
-                $result = $app->onBefore() . $result;
-            }
-
-            // Prepend content provided by apps onAfter() event method
-            if (method_exists($app, 'onAfter')) {
-                $result .= $app->onAfter();
-            }
-        }
-        catch (Exception $e) {
-
-            $result = $di->get('core.error')->handleException($e);
-        }
-
-        $content->setContent($result);
-
-        // Call content builder
-        echo $content->render();
-
-        $fb = [
-            'Runtime' => $timer->getDiff() . 's',
-            'Permissions' => $di->get('core.sec.permission')->getPermissions(),
-            'Router match' => $router->match()
-        ];
-
-        $debug = $content->getDebug();
-
-        if ($debug) {
-            $fb['Content debug'] = $debug;
-        }
-
-        \FB::log($fb);
-    }
 }
-
-// # Error handling
 catch (Exception $e) {
     echo $di->get('core.error')->handleException($e, true);
 }
