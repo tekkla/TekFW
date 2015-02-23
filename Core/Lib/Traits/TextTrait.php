01e9f3e8b99c4882c39b3893a1e9dfd1ba2df84d
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-23 12:24:34 +0100

    Code extension to check for $app_name property
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/Lib/Traits/TextTrait.php
+++ Core/Lib/Traits/TextTrait.php
@@ -7,7 +7,7 @@ use Core\Lib\Amvc\App;
  * Text Trait
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- * @copyright 2014
+ * @copyright 2015
  * @license MIT
  */
 trait TextTrait
@@ -25,14 +25,20 @@ trait TextTrait
     {
         global $di;
 
-        if ($this instanceof App) {
-            $app = $this->name;
-        }
-        elseif (property_exists($this, 'app')) {
-            $app = $this->app->getName();
+        if (! property_exists($this, 'app_name')) {
+
+            if ($this instanceof App) {
+                $app = $this->name;
+            }
+            elseif (property_exists($this, 'app')) {
+                $app = $this->app->getName();
+            }
+            else {
+                $app = 'Core';
+            }
         }
         else {
-            $app = 'Core';
+            $app = $this->app_name;
         }
 
         return $di->get('core.content.lang')->getTxt($key, $app);
