cb8ac03742df80966d47c84e2d8c0b26f4e472c1
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-23 20:20:01 +0100

    Added comment and made injectAdapter() method final.
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/Lib/Data/Adapter/AdapterAbstract.php
+++ Core/Lib/Data/Adapter/AdapterAbstract.php
@@ -2,22 +2,34 @@
 namespace Core\Lib\Data\Adapter;
 
 use Core\Lib\Data\DataAdapter;
+
 /**
+ * DataAdapert abstract class
  *
- * @author Michael
- *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @license MIT
+ * @copyright 2015 by author
  */
 abstract class AdapterAbstract
 {
 
-	/**
-	 *
-	 * @var DataAdapter
-	 */
-	protected $adapter;
+    /**
+     *
+     * @var DataAdapter
+     */
+    protected $adapter;
 
-	public function injectAdapter(DataAdapter $adapter)
-	{
-		$this->adapter = $adapter;
-	}
+    /**
+     * Injects DataAdapter into adapter class
+     *
+     * @param DataAdapter $adapter
+     *
+     * @return Database|Xml|Json
+     */
+    final public function injectAdapter(DataAdapter $adapter)
+    {
+        $this->adapter = $adapter;
+
+        return $this;
+    }
 }
