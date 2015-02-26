285a07cf3b7192026e3cbf29a0932b95160faa74
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-02-26 02:00:30 +0100

    NEW Nav html element
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

---
+++ Core/Lib/Content/Html/Elements/Nav.php
@@ -0,0 +1,19 @@
+<?php
+namespace Core\Lib\Content\Html\Elements;
+
+use Core\Lib\Content\Html\HtmlAbstract;
+
+/**
+ * Nav Html Element
+ *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @package TekFW
+ * @subpackage Html\Elements
+ * @license MIT
+ * @copyright 2015 by author
+ */
+class Nav extends HtmlAbstract
+{
+
+	protected $element = 'nav';
+}
