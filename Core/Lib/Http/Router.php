4e1594b9a0d881563e5f57091d6738db31c9b323
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-03-08 21:48:34 +0100

    Add raw matchresult to getStatus() method
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/Lib/Http/Router.php
+++ Core/Lib/Http/Router.php
@@ -1,352 +1,364 @@
 <?php
 namespace Core\Lib\Http;
 
+use Core\Lib\Traits\StringTrait;
+use Core\Lib\Traits\ConvertTrait;
+
 /**
  * Router class which handles routes and request like post or get.
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- * @copyright 2014
+ * @copyright 2015
  * @license MIT
- * @package TekFW
- * @subpackage Lib
  */
 final class Router extends \AltoRouter
 {
-	use \Core\Lib\Traits\StringTrait;
-	use \Core\Lib\Traits\ConvertTrait;
+    use StringTrait;
+    use ConvertTrait;
 
-	/**
-	 * Status flag ajax
-	 *
-	 * @var bool
-	 */
-	private $is_ajax = false;
+    /**
+     * Status flag ajax
+     *
+     * @var bool
+     */
+    private $is_ajax = false;
 
-	/**
-	 * Routered app
-	 *
-	 * @var string
-	 */
-	private $app = '';
+    /**
+     * Routered app
+     *
+     * @var string
+     */
+    private $app = '';
 
-	/**
-	 * Routered conroller
-	 *
-	 * @var string
-	 */
-	private $ctrl = '';
+    /**
+     * Routered conroller
+     *
+     * @var string
+     */
+    private $ctrl = '';
 
-	/**
-	 * Routeret action
-	 *
-	 * @var string
-	 */
-	private $action = '';
+    /**
+     * Routeret action
+     *
+     * @var string
+     */
+    private $action = '';
 
-	/**
-	 * Target parameter used in AJAX requestshandling
-	 *
-	 * @var string
-	 */
-	private $target = '';
+    /**
+     * Target parameter used in AJAX requestshandling
+     *
+     * @var string
+     */
+    private $target = '';
 
-	/**
-	 * storage for GET parameters
-	 *
-	 * @var array
-	 */
-	private $params = [];
+    /**
+     * storage for GET parameters
+     *
+     * @var array
+     */
+    private $params = [];
 
-	/**
-	 * Name of current route
-	 *
-	 * @var string
-	 */
-	private $name = '';
-
-	// ---------------------------------------------------------------------------
-	// ROUTE HANDLING
-	// ---------------------------------------------------------------------------
-
-	/**
-	 * Reversed routing
-	 * Generate the URL for a named route.
-	 * Replace regexes with supplied parameters
-	 *
-	 * @param string $route_name The name of the route.
-	 * @param array @params Associative array of parameters to replace placeholders with.
-	 * @return string The URL of the route with named parameters in place.
-	 * @todo Add access check
-	 */
-	public function url($route_name, $params = [])
-	{
-		return $this->generate($route_name, $params);
-	}
-
-	/**
-	 * Match a given request url against stored routes
-	 *
-	 * @param string $request_url
-	 * @param string $request_method
-	 *
-	 * @return array boolean with route information on success, false on failure (no match).
-	 */
-	public function match($request_url = null, $request_method = null)
-	{
-
-		// Set Request Url if it isn't passed as parameter
-		if($request_url === null) {
-			$request_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
-		}
-
-		// Set Request Method if it isn't passed as a parameter
-		if($request_method === null) {
-			$request_method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
-		}
-
-		// Framework ajax.js adds automatically an /ajax flag @ the end of the requested URI.
-		// Here we check for this flag, remembers if it's present and then remove the flag
-		// so the following URI matching process runs without flaw.
-		if (substr($request_url, -5) == '/ajax')
-		{
-			$this->is_ajax = true;
-			$request_url = str_replace('/ajax', '', $request_url);
-		}
-
-		// Try to match request
-		$match = parent::match($request_url, $request_method);
-
-		if ($match)
-		{
-			if (isset($match['name'])) {
-				$this->name = $match['name'];
-			}
-
-			// Map target results to request properties
-			foreach ($match['target'] as $key => $val) {
-				if (property_exists($this, $key)) {
-					$this->{$key} = $this->camelizeString($val);
-				}
-			}
-
-			// When no target ctrl defined in route but provided by parameter
-			// we use the parameter as requested ctrl
-			if (! $this->ctrl && isset($match['params']['ctrl'])) {
-				$this->ctrl = $this->camelizeString($match['params']['ctrl']);
-			}
-
-			// Same for action as for ctrl
-			if (! $this->action && isset($match['params']['action'])) {
-				$this->action = $this->camelizeString($match['params']['action']);
-			}
-
-			// Is this an ajax request?
-			if (isset($match['params']['ajax'])) {
-				$this->is_ajax = true;
-			}
-
-			$this->params = $match['params'];
-		}
-
-		return $match;
-	}
-
-	/**
-	 * Returns the name of the current active route
-	 */
-	public function getCurrentRoute()
-	{
-		return $this->name;
-	}
+    /**
+     * Name of current route
+     *
+     * @var string
+     */
+    private $name = '';
 
 
-	/**
-	 * Checks for an ajax request and returns boolean true or false
-	 *
-	 * @return boolean
-	 */
-	public function isAjax()
-	{
-		return $this->is_ajax;
-	}
 
+    private $raw = false;
 
-	/**
-	 * Checks if the request is a (A)pp(C)ontroller(A)ction call
-	 *
-	 * @return boolean
-	 */
-	public function isCall()
-	{
-		return $this->checkApp() && $this->checkCtrl() && $this->checkAction();
-	}
+    /**
+     * Reversed routing
+     * Generate the URL for a named route.
+     * Replace regexes with supplied parameters
+     *
+     * @param string $route_name The name of the route.
+     * @param array @params Associative array of parameters to replace placeholders with.
+     * @return string The URL of the route with named parameters in place.
+     * @todo Add access check
+     */
+    public function url($route_name, $params = [])
+    {
+        return $this->generate($route_name, $params);
+    }
 
-	/**
-	 * Check set of app property
-	 */
-	public function checkApp()
-	{
-		return isset($this->app);
-	}
+    /**
+     * Match a given request url against stored routes
+     *
+     * @param string $request_url
+     * @param string $request_method
+     *
+     * @return array boolean with route information on success, false on failure (no match).
+     */
+    public function match($request_url = null, $request_method = null)
+    {
 
-	/**
-	 * Returns a camelized app name
-	 *
-	 * @return string
-	 */
-	public function getApp()
-	{
-		return $this->app ? $this->app : false;
-	}
+        // Set Request Url if it isn't passed as parameter
+        if ($request_url === null) {
+            $request_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+        }
 
-	/**
-	 * Set appname manually
-	 *
-	 * @param string $val
-	 */
-	public function setApp($app)
-	{
-		$this->app = $app;
-		return $this;
-	}
+        // Set Request Method if it isn't passed as a parameter
+        if ($request_method === null) {
+            $request_method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+        }
 
-	/**
-	 * Checks if a ctrl is set and returns true/false
-	 *
-	 * @return boolean
-	 */
-	public function checkCtrl()
-	{
-		return ! empty($this->ctrl);
-	}
+        // Framework ajax.js adds automatically an /ajax flag @ the end of the requested URI.
+        // Here we check for this flag, remembers if it's present and then remove the flag
+        // so the following URI matching process runs without flaw.
+        if (substr($request_url, - 5) == '/ajax') {
+            $this->is_ajax = true;
+            $request_url = str_replace('/ajax', '', $request_url);
+        }
 
-	/**
-	 * Returns a camelized ctrl name
-	 *
-	 * @return string
-	 */
-	public function getCtrl()
-	{
-		return $this->ctrl;
-	}
+        $this->raw = parent::match($request_url, $request_method);
 
-	/**
-	 * Sets the requested ctrl manually
-	 *
-	 * @param string $ctrl
-	 */
-	public function setCtrl($ctrl)
-	{
-		$this->ctrl = $ctrl;
-		return $this;
-	}
+        // Try to match request
+        $match = $this->raw;
 
-	/**
-	 * Checks if the function name is set
-	 */
-	public function checkAction()
-	{
-		return ! empty($this->action);
-	}
+        if ($match) {
 
-	/**
-	 * Returns either the requested or 'Index' (default) as action name
-	 *
-	 * @todo Routerhandler should not set a default action!
-	 */
-	public function getAction()
-	{
-		return $this->action;
-	}
+            if (isset($match['name'])) {
+                $this->name = $match['name'];
+            }
 
-	/**
-	 * Set the requested func manually
-	 *
-	 * @param string $action
-	 */
-	public function setAction($action)
-	{
-		$this->action = $action;
-		return $this;
-	}
+            // Map target results to request properties
+            foreach ($match['target'] as $key => $val) {
+                if (property_exists($this, $key)) {
+                    $this->{$key} = $this->camelizeString($val);
+                }
+            }
 
-	/**
-	 * Returns requested app, ctrl and action as array
-	 *
-	 * @return array
-	 */
-	public function getACA()
-	{
-		return [
-			'app' => $this->app,
-			'ctrl' => $this->ctrl,
-			'action' => $this->action
-		];
-	}
+            // When no target ctrl defined in route but provided by parameter
+            // we use the parameter as requested ctrl
+            if (! $this->ctrl && isset($match['params']['ctrl'])) {
+                $this->ctrl = $this->camelizeString($match['params']['ctrl']);
+            }
 
-	/**
-	 * Boolean check if parameter exists
-	 *
-	 * @param string parametername
-	 * @return boolean
-	 */
-	public function checkParam($key)
-	{
-		return isset($this->params[$key]);
-	}
+            // Same for action as for ctrl
+            if (! $this->action && isset($match['params']['action'])) {
+                $this->action = $this->camelizeString($match['params']['action']);
+            }
 
-	/**
-	 * Sets one or mor request parameter.
-	 * Set $arg1 as assoc array to add multiple parameter.
-	 * Both args ($arg1 and $arg2) set means to add a parameter by key ($arg1) and value ($arg2)
-	 *
-	 * @param string|array $arg1
-	 * @param string $arg2
-	 * @return \Core\Lib\Router
-	 */
-	public function addParam($arg1, $arg2 = null)
-	{
-		if ($arg2 === null && is_array($arg1)) {
-			$arg1 = $this->convertObjectToArray($arg1);
+            // Is this an ajax request?
+            if (isset($match['params']['ajax'])) {
+                $this->is_ajax = true;
+            }
 
-			foreach ($arg1 as $key => $val)
-				$this->params[$key] = $val;
-		}
+            $this->params = $match['params'];
+        }
 
-		if ($arg2 !== null)
-			$this->params[$arg1] = $this->convertObjectToArray($arg2);
+        return $match;
+    }
 
-		return $this;
-	}
+    /**
+     * Returns the name of the current active route
+     */
+    public function getCurrentRoute()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * Returns the value of a paramter as long as it exists.
-	 *
-	 * @param string $key
-	 */
-	public function getParam($key = null)
-	{
-		if (isset($key)) {
-			return isset($this->params[$key]) ? $this->params[$key] : false;
-		}
+    /**
+     * Checks for an ajax request and returns boolean true or false
+     *
+     * @return boolean
+     */
+    public function isAjax()
+    {
+        return $this->is_ajax;
+    }
 
-		return $this->params;
-	}
+    /**
+     * Checks if the request is a (A)pp(C)ontroller(A)ction call
+     *
+     * @return boolean
+     */
+    public function isCall()
+    {
+        return $this->checkApp() && $this->checkCtrl() && $this->checkAction();
+    }
 
+    /**
+     * Check set of app property
+     */
+    public function checkApp()
+    {
+        return isset($this->app);
+    }
 
-	/**
-	 * Returns all request related data in one array
-	 * @return array
-	 */
-	public function getStatus()
-	{
-		return [
-			'is_ajax' => $this->is_ajax,
-			'method' => $_SERVER['REQUEST_METHOD'],
-			'app' => $this->getApp(),
-			'ctrl' => $this->getCtrl(),
-			'action' => $this->getAction(),
-			'params' => $this->getAllParams()
-		];
-	}
+    /**
+     * Returns a camelized app name
+     *
+     * @return string
+     */
+    public function getApp()
+    {
+        return $this->app ? $this->app : false;
+    }
+
+    /**
+     * Set appname manually
+     *
+     * @param string $val
+     */
+    public function setApp($app)
+    {
+        $this->app = $app;
+
+        return $this;
+    }
+
+    /**
+     * Checks if a ctrl is set and returns true/false
+     *
+     * @return boolean
+     */
+    public function checkCtrl()
+    {
+        return ! empty($this->ctrl);
+    }
+
+    /**
+     * Returns a camelized ctrl name
+     *
+     * @return string
+     */
+    public function getCtrl()
+    {
+        return $this->ctrl;
+    }
+
+    /**
+     * Sets the requested ctrl manually
+     *
+     * @param string $ctrl
+     */
+    public function setCtrl($ctrl)
+    {
+        $this->ctrl = $ctrl;
+
+        return $this;
+    }
+
+    /**
+     * Checks if the function name is set
+     */
+    public function checkAction()
+    {
+        return ! empty($this->action);
+    }
+
+    /**
+     * Returns either the requested or 'Index' (default) as action name
+     *
+     * @todo Routerhandler should not set a default action!
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * Set the requested func manually
+     *
+     * @param string $action
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * Returns requested app, ctrl and action as array
+     *
+     * @return array
+     */
+    public function getACA()
+    {
+        return [
+            'app' => $this->app,
+            'ctrl' => $this->ctrl,
+            'action' => $this->action
+        ];
+    }
+
+    /**
+     * Boolean check if parameter exists
+     *
+     * @param string parametername
+     * @return boolean
+     */
+    public function checkParam($key)
+    {
+        return array_key_exists($key, $his->params);
+    }
+
+    /**
+     * Sets one or mor request parameter.
+     * Set $arg1 as assoc array to add multiple parameter.
+     * Both args ($arg1 and $arg2) set means to add a parameter by key ($arg1) and value ($arg2)
+     *
+     * @param string|array $arg1
+     * @param string $arg2
+     * @return \Core\Lib\Router
+     */
+    public function addParam($arg1, $arg2 = null)
+    {
+        if ($arg2 === null && is_array($arg1)) {
+
+            $arg1 = $this->convertObjectToArray($arg1);
+
+            foreach ($arg1 as $key => $val) {
+                $this->params[$key] = $val;
+            }
+        }
+
+        if ($arg2 !== null) {
+            $this->params[$arg1] = $this->convertObjectToArray($arg2);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of a paramter as long as it exists.
+     *
+     * @param string $key
+     */
+    public function getParam($key = null)
+    {
+        if (isset($key)) {
+            return isset($this->params[$key]) ? $this->params[$key] : false;
+        }
+
+        return $this->params;
+    }
+
+    /**
+     * Returns all request related data in one array
+     *
+     * @return array
+     */
+    public function getStatus()
+    {
+        return [
+            'is_ajax' => $this->is_ajax,
+            'method' => $_SERVER['REQUEST_METHOD'],
+            'app' => $this->getApp(),
+            'ctrl' => $this->getCtrl(),
+            'action' => $this->getAction(),
+            'params' => $this->getParam(),
+            'raw' => $this->raw
+        ];
+    }
+
+    public function clearPost()
+    {
+        unset($_POST);
+    }
 }
