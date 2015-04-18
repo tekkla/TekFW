9f2235db29d28ece1035a08fa8521d5f482a8880
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-04-18 23:34:08 +0200

    Sets some default meta tags like UTF-8 charset and viewpors. Also reformats code.
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/Lib/Content/Meta.php
+++ Core/Lib/Content/Meta.php
@@ -10,134 +10,143 @@ namespace Core\Lib\Content;
  */
 class Meta
 {
-	/**
-	 * Tags storage
-	 *
-	 * @var array
-	 */
-	private $tags = [];
 
-	/**
-	 * Adds a generic tag
-	 *
-	 * @param array $properties
-	 */
-	public function setGenericTag($properties)
-	{
-		$this->tags[] = $properties;
-	}
+    /**
+     * Tags storage
+     *
+     * @var array
+     */
+    private $tags = [
+        'charset' => [
+            'charset' => 'UTF-8'
+        ],
+        'viewport' => [
+            'name' => 'viewport',
+            'content' => 'width=device-width, initial-scale=1'
+        ]
+    ];
 
-	public function setViewport($width='device-width', $initial_scale = '1', $user_scalable='', $minimum_scale = '', $maximum_scale = '')
-	{
-		$tag = [
-			'name' => 'viewport',
-			'content' => 'width=' . $width . ', initial-scale=' . $initial_scale
-		];
+    /**
+     * Adds a generic tag
+     *
+     * @param array $properties
+     */
+    public function setGenericTag($properties)
+    {
+        $this->tags[] = $properties;
+    }
 
-		if ($user_scalable) {
-			$tag['content'] .= ', user-scalable=' . $user_scalable;
-		}
+    public function setViewport($width = 'device-width', $initial_scale = '1', $user_scalable = '', $minimum_scale = '', $maximum_scale = '')
+    {
+        $tag = [
+            'name' => 'viewport',
+            'content' => 'width=' . $width . ', initial-scale=' . $initial_scale
+        ];
 
-		if ($minimum_scale) {
-			$tag['content'] .= ', minimum-scale=' . $minimum_scale;
-		}
+        if ($user_scalable) {
+            $tag['content'] .= ', user-scalable=' . $user_scalable;
+        }
 
-		if ($maximum_scale) {
-			$tag['content'] .= ', maximum_scale=' . $maximum_scale;
-		}
+        if ($minimum_scale) {
+            $tag['content'] .= ', minimum-scale=' . $minimum_scale;
+        }
 
-		$this->tags['viewport'] = $tag;
-	}
+        if ($maximum_scale) {
+            $tag['content'] .= ', maximum_scale=' . $maximum_scale;
+        }
 
-	/**
-	 * Sets charset tag
-	 *
-	 * @param string $charset
-	 *
-	 * @return \Core\Lib\Content\Meta
-	 */
-	public function setCharset($charset = 'UTF-8')
-	{
-		$this->tags['charset'] = [
-			'charset' => $charset
-		];
+        $this->tags['viewport'] = $tag;
+    }
 
-		return $this;
-	}
+    /**
+     * Sets charset tag
+     *
+     * @param string $charset
+     *
+     * @return \Core\Lib\Content\Meta
+     */
+    public function setCharset($charset = 'UTF-8')
+    {
+        $this->tags['charset'] = [
+            'charset' => $charset
+        ];
 
-	/**
-	 * Sets description tag
-	 *
-	 * @param string $description
-	 *
-	 * @return \Core\Lib\Content\Meta
-	 */
-	public function setDescription($description='')
-	{
-		$this->tags['description'] = [
-			'name' => 'description',
-			'content' => $description
-		];
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Sets description tag
+     *
+     * @param string $description
+     *
+     * @return \Core\Lib\Content\Meta
+     */
+    public function setDescription($description = '')
+    {
+        $this->tags['description'] = [
+            'name' => 'description',
+            'content' => $description
+        ];
 
-	/**
-	 * Sets keywords tag
-	 *
-	 * @param string $keywords
-	 *
-	 * @return \Core\Lib\Content\Meta
-	 */
-	public function setKeywords($keywords='')
-	{
-		$this->tags['keywords'] = [
-			'name' => 'keywords',
-			'content' => $keywords
-		];
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Sets keywords tag
+     *
+     * @param string $keywords
+     *
+     * @return \Core\Lib\Content\Meta
+     */
+    public function setKeywords($keywords = '')
+    {
+        $this->tags['keywords'] = [
+            'name' => 'keywords',
+            'content' => $keywords
+        ];
 
-	/**
-	 * Sets author tag
-	 *
-	 * @param string $author
-	 *
-	 * @return \Core\Lib\Content\Meta
-	 */
-	public function setAuthor($author='')
-	{
-		$this->tags['author'] = [
-			'name' => 'author',
-			'content' => $author
-		];
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Sets author tag
+     *
+     * @param string $author
+     *
+     * @return \Core\Lib\Content\Meta
+     */
+    public function setAuthor($author = '')
+    {
+        $this->tags['author'] = [
+            'name' => 'author',
+            'content' => $author
+        ];
 
-	/**
-	 * Sets http-equiv refresh tag
-	 *
-	 * @param number $refresh
-	 *
-	 * @return \Core\Lib\Content\Meta
-	 */
-	public function setRefresh($refresh = 30)
-	{
-		$this->tags['http-equiv'] = [
-			'http-equiv' => 'refresh',
-			'content' => $refresh
-		];
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * Sets http-equiv refresh tag
+     *
+     * @param number $refresh
+     *
+     * @return \Core\Lib\Content\Meta
+     */
+    public function setRefresh($refresh = 30)
+    {
+        $this->tags['http-equiv'] = [
+            'http-equiv' => 'refresh',
+            'content' => $refresh
+        ];
 
-	/**
-	 * Returns all set tags
-	 */
-	public function getTags()
-	{
-		return $this->tags;
-	}
+        return $this;
+    }
+
+    /**
+     * Returns all set tags
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
 }
