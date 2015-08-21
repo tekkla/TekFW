f598647202c1b090028de6ead8f4cd4a17f342af
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-08-21 13:14:05 +0200

    Encloses login form and status in a centered container div
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Core/AppsSec/Core/View/SecurityView.php
+++ Core/AppsSec/Core/View/SecurityView.php
@@ -13,12 +13,20 @@ final class SecurityView extends View
 
     public function Login()
     {
-        echo $this->form;
+        echo '
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">', $this->form , '</div>
+        </div>';
     }
 
     public function AlreadyLoggedIn()
     {
-        echo $this->loggedin;
+        echo '
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <strong>', $this->loggedin, '</strong>
+            </div>
+        </div>';
     }
 }
 
