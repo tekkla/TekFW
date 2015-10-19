18112367f40ac87849bb8ad03667e01b6c154b9e
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-10-20 01:55:57 +0200

    Adds Bootstrap collapsable support
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

--- Themes/Core/IndexTemplate.php
+++ Themes/Core/IndexTemplate.php
@@ -64,7 +64,7 @@ class IndexTemplate extends Template
 <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -72,7 +72,7 @@ class IndexTemplate extends Template
             </button>
             <a class="navbar-brand" href="/">', $this->content->getBrand() , '</a>
         </div>
-        <div class="collapse navbar-collapse">';
+        <div class="collapse navbar-collapse" id="navbar">';
 
             $service = $this->getMenu('service');
 
