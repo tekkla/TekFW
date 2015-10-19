e38a10fb471c24cdc449680587b200f438d30d20
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2015-10-20 01:58:45 +0200

    Adds new Container objects
    Signed-off-by: Michael "Tekkla" Zorn <tekkla@tekkla.de>

---
+++ Core/AppsSec/Core/Container/ConfigContainer.php
@@ -0,0 +1,16 @@
+<?php
+namespace Core\AppsSec\Core\Container;
+
+use Core\Lib\Data\Container;
+
+/**
+ * ConfigContainer.php
+ *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @copyright 2015
+ * @license MIT
+ */
+class ConfigContainer extends Container
+{
+    public $available = [];
+}
---
+++ Core/AppsSec/Core/Container/CoreContainer.php
@@ -0,0 +1,17 @@
+<?php
+namespace Core\AppsSec\Core\Container;
+
+use Core\Lib\Data\Container;
+
+/**
+ * CoreContainer.php
+ *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @copyright 2015
+ * @license MIT
+ */
+class CoreContainer extends Container
+{
+    protected $available = [];
+
+}
