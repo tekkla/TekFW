1be82d17001ca6801ad845a7c14ca98acc34702d
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2016-02-04 18:01:12 -0400

    Updates theme
    * Changes path of Template parent class
    * Updates Bootstrap to latest 3.3.6
    * Updates Fontawesome to latest 4.5.0
    * Adds minor classes to themes css file

--- Themes/Core/IndexTemplate.php
+++ Themes/Core/IndexTemplate.php
@@ -1,8 +1,15 @@
 <?php
 namespace Themes\Core;
 
-use Core\Lib\Content\Template;
+use Core\Lib\Page\Template;
 
+/**
+ * IndexTemplate.php
+ *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @copyright 2016
+ * @license MIT
+ */
 class IndexTemplate extends Template
 {
 
@@ -40,17 +47,19 @@ class IndexTemplate extends Template
         echo '
         <body>';
 
-            // Navbar
-            $this->createMenu();
+        // Navbar
+        $this->createMenu();
 
-            // Message container
-            echo '<div id="core-message" class="container">', $this->getMessages(), '</div>';
+        // Message container
+        echo $this->getMessages();
 
-            // Main content
-            echo '<div id="content" class="container">', $this->getContent(), '</div>',
+        // Main content
+        echo $this->getContent();
 
-    		$this->getScript('below'),
-	   '</body>';
+        echo $this->getScript('below');
+
+        echo '
+        </body>';
     }
 
     public function htmlBelow()
@@ -70,59 +79,63 @@ class IndexTemplate extends Template
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="', $this->content->getBaseUrl() , '">', $this->content->getBrand() , '</a>
+            <a class="navbar-brand" href="', $this->page->getBaseUrl(), '">', $this->page->getBrand(), '</a>
         </div>
         <div class="collapse navbar-collapse" id="navbar">';
 
-            $service = $this->getMenu('service');
+        $service = $this->getMenu('service');
 
-            if ($service) {
-                echo '<ul class="nav navbar-nav">';
+        if ($service) {
+            echo '<ul class="nav navbar-nav">';
 
-                foreach($service->getItems() as $item) {
-                    echo '<li><a data-ajax href="',  $item->geTurl(), '">', $item->getText(), '</a></li>';
-                }
-
-                echo '</ul>';
+            foreach ($service->getItems() as $item) {
+                echo '<li><a data-ajax href="', $item->geTurl(), '">', $item->getText(), '</a></li>';
             }
 
-            //  Add admin menu and login button
-            $admin = $this->getMenu('admin');
-            $login = $this->getMenu('login');
+            echo '</ul>';
+        }
 
-            if ($admin || $login) {
+        // Add admin menu and login button
+        $admin = $this->getMenu('admin');
+        $register = $this->getMenu('register');
+        $login = $this->getMenu('login');
 
-                echo'
+        if ($admin || $register || $login) {
+
+            echo '
                 <ul class="nav navbar-nav navbar-right">';
 
-                    if ($admin) {
-                        echo '
+            if ($admin) {
+                echo '
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">', $admin->getText() ,' <span class="caret"></span></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">', $admin->getText(), ' <span class="caret"></span></a>
                             <ul class="dropdown-menu">';
 
-                            foreach($admin->getItems() as $item) {
-                                echo '<li><a href="',  $item->getUrl(), '">', $item->getText(), '</a></li>';
-                            }
-
-                            echo '
-                            </ul>
-                        </li>';
-                    }
-
-                    if ($login) {
-                        echo '<li><a href="',  $login->getUrl(), '">', $login->getText(), '</a></li>';
-                    }
+                foreach ($admin->getItems() as $item) {
+                    echo '<li><a href="', $item->getUrl(), '">', $item->getText(), '</a></li>';
+                }
 
                 echo '
-                </ul>';
+                            </ul>
+                        </li>';
+            }
+
+            if ($register) {
+                echo '<li><a href="', $register->getUrl(), '">', $register->getText(), '</a></li>';
+            }
+
+            if ($login) {
+                echo '<li><a href="', $login->getUrl(), '">', $login->getText(), '</a></li>';
             }
 
             echo '
+                </ul>';
+        }
+
+        echo '
         </div>
     </div>
 </nav>';
-
     }
 }
 
