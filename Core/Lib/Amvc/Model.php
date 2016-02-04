8f4569d6284c18a1608c6b66792621da7fde7638
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2016-02-04 14:47:51 -0400

    Changes model to use it's own ModelException and removes ArrayAccess

--- Core/Lib/Amvc/Model.php
+++ Core/Lib/Amvc/Model.php
@@ -1,27 +1,27 @@
 <?php
 namespace Core\Lib\Amvc;
 
-use Core\Lib\Traits\SerializeTrait;
-use Core\Lib\Traits\ArrayTrait;
-use Core\Lib\Traits\UrlTrait;
+// DataLibs
 use Core\Lib\Data\Connectors\Db\Db;
-use Core\Lib\Data\Container;
-use Core\Lib\Errors\Exceptions\InvalidArgumentException;
-;
+use Core\Lib\Data\Container\Container;
+
+// Traits
+use Core\Lib\Traits\ArrayTrait;
+use Core\Lib\Router\UrlTrait;
+use Core\Lib\Cfg\CfgTrait;
 
 /**
  * Model.php
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- * @copyright 2015
+ * @copyright 2016
  * @license MIT
  */
-class Model extends MvcAbstract implements \ArrayAccess
+class Model extends MvcAbstract
 {
-
-    use SerializeTrait;
     use ArrayTrait;
     use UrlTrait;
+    use CfgTrait;
 
     /**
      * MVC component type
@@ -29,8 +29,6 @@ class Model extends MvcAbstract implements \ArrayAccess
      * @var string
      */
     protected $type = 'Model';
-
-    protected $data = false;
 
     /**
      * Constructor
@@ -44,23 +42,6 @@ class Model extends MvcAbstract implements \ArrayAccess
         // Set Properties
         $this->name = $name;
         $this->app = $app;
-    }
-
-    /**
-     * Access to the apps config.
-     *
-     * Without any paramter set this method returns the complete config.
-     * With only key set, it returns the value associated with it.
-     * Set key and value, and the config will be updated.
-     *
-     * @param string $key
-     * @param string $val
-     *
-     * @return mixed
-     */
-    final public function cfg($key = null, $val = null)
-    {
-        return $this->app->cfg($key, $val);
     }
 
     /**
@@ -87,11 +68,13 @@ class Model extends MvcAbstract implements \ArrayAccess
      * Creates an app related container
      *
      * @param string $container_name
-     *            Optional: Name of the container to load. When no name is given the name of the current model will be used.
+     *            Optional: Name of the container to load. When no name is given the name of the current model will be
+     *            used.
      * @param bool $auto_init
-     *            Optional: Autoinit uses the requested action to fill the container with according fields by calling the same called method of container.
+     *            Optional: Autoinit uses the requested action to fill the container with according fields by calling
+     *            the same called method of container.
      *
-     * @return \Core\Lib\Data\Container
+     * @return \Core\Lib\Data\Container\Container
      */
     final public function getContainer($container_name = '')
     {
@@ -105,7 +88,7 @@ class Model extends MvcAbstract implements \ArrayAccess
     /**
      * Creates an generic container object.
      *
-     * @return \Core\Lib\Data\Container
+     * @return \Core\Lib\Data\Container\Container
      */
     final public function getGenericContainer($fields = [])
     {
@@ -133,7 +116,7 @@ class Model extends MvcAbstract implements \ArrayAccess
     final protected function getDbConnector($resource_name = 'db.default', $prefix = '', $fields = [])
     {
         if (! $this->di->exists($resource_name)) {
-            Throw new InvalidArgumentException(sprintf('A database service with name "%s" ist not registered', $resource_name));
+            Throw new ModelException(sprintf('A database service with name "%s" ist not registered', $resource_name));
         }
 
         /* @var $db Db */
@@ -153,47 +136,5 @@ class Model extends MvcAbstract implements \ArrayAccess
         }
 
         return $db;
-    }
-
-    /**
-     * (non-PHPdoc)
-     *
-     * @see ArrayAccess::offsetSet()
-     */
-    public function offsetSet($offset, $value)
-    {
-        if (! is_null($offset)) {
-            $this->data[$offset] = $value;
-        }
-    }
-
-    /**
-     * (non-PHPdoc)
-     *
-     * @see ArrayAccess::offsetExists()
-     */
-    public function offsetExists($offset)
-    {
-        return isset($this->data[$offset]);
-    }
-
-    /**
-     * (non-PHPdoc)
-     *
-     * @see ArrayAccess::offsetUnset()
-     */
-    public function offsetUnset($offset)
-    {
-        unset($this->data[$offset]);
-    }
-
-    /**
-     * (non-PHPdoc)
-     *
-     * @see ArrayAccess::offsetGet()
-     */
-    public function offsetGet($offset)
-    {
-        return isset($this->data[$offset]) ? $this->data[$offset] : null;
     }
 }
