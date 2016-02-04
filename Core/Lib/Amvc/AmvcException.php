0bfd6f7edad630dfd6cb532f0905307b1c4e5917
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2016-02-04 14:42:24 -0400

    Adds new AMVC related exceptions

---
+++ Core/Lib/Amvc/AmvcException.php
@@ -0,0 +1,15 @@
+<?php
+namespace Core\Lib\Amvc;
+
+use Core\Lib\Errors\Exceptions\BasicException;
+
+/**
+ * AmvcException.php
+ *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @copyright 2016
+ * @license MIT
+ */
+class AmvcException extends BasicException
+{
+}
