46cf17bb2229369d6dbdddc6e6e37cb1c7e3402d
Michael Tekkla Zorn <tekkla@tekkla.de>
2016-05-09 03:01:48 +0200

    New interface with Bootstrap contextual color contants
    Signed-off-by: Michael Tekkla Zorn <tekkla@tekkla.de>

---
+++ Core/Html/Bootstrap/BootstrapContextInterface.php
@@ -0,0 +1,22 @@
+<?php
+namespace Core\Html\Bootstrap;
+
+/**
+ * BootstrapContextInterface.php
+ *
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @copyright 2016
+ * @license MIT
+ */
+interface BootstrapContextInterface
+{
+    const PRIMARY = 'primary';
+
+    const SUCCESS = 'success';
+
+    const INFO = 'info';
+
+    const WARNING = 'warning';
+
+    const DANGER = 'danger';
+}
